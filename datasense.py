f7002c1da2c4e5f0e292194266f5409a90a3ff39
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-09-17 10:38:32 -0500

    Fix errors in datasense.py

--- datasense.py
+++ datasense.py
@@ -1,55 +1,60 @@
-"""
-Return five statistics
+import pandas as pd
 
-Returns
--------
-min            = minimum value
-quantile(0.25) = first quartile
-quantile(0.50) = median
-quantile(0.75) = third quartile
-max            = maximum value
-"""
 
 def five_number_summary(data: pd.Series) -> pd.DataFrame:
-   return pd.DataFrame([(interpolation,
-        data.min(),
-        data.quantile(0.25, interpolation=interpolation),
-        data.quantile(0.50, interpolation=interpolation),
-        data.quantile(0.75, interpolation=interpolation),
-        data.max())
-        for interpolation
-            in ('linear', 'lower', 'higher', 'nearest',
-                'midpoint')],
-            columns=['interpolation', 'min', 'q1', 'q2',
-                     'q3', 'max']).\
-            set_index(['interpolation'])
+    """
+    Return five statistics
 
-"""
-Return six statistics
+    Returns
+    -------
+    min            = minimum value
+    quantile(0.25) = first quartile
+    quantile(0.50) = median
+    quantile(0.75) = third quartile
+    max            = maximum value
+    """
+    return pd.DataFrame(
+        [(interpolation,
+          data.min(),
+          data.quantile(0.25, interpolation=interpolation),
+          data.quantile(0.50, interpolation=interpolation),
+          data.quantile(0.75, interpolation=interpolation),
+          data.max())
+         for interpolation
+         in ('linear', 'lower', 'higher', 'nearest',
+             'midpoint')],
+        columns=['interpolation', 'min', 'q1', 'q2',
+                 'q3', 'max']
+    ).set_index(['interpolation'])
 
-Returns
--------
-min            = minimum value
-quantile(0.25) = first quartile
-quantile(0.50) = median
-quantile(0.75) = third quartile
-max            = maximum value
-iqr            = interquartile range
-"""
 
 def six_number_summary(data: pd.Series) -> pd.DataFrame:
-   return pd.DataFrame([(interpolation,
-        data.min(),
-        data.quantile(0.25, interpolation=interpolation),
-        data.quantile(0.50, interpolation=interpolation),
-        data.quantile(0.75, interpolation=interpolation),
-        data.max(),
-        (data.quantile(0.75, interpolation=interpolation) -\
-         data.quantile(0.25, interpolation=interpolation))
-        )
-        for interpolation
-            in ('linear', 'lower', 'higher', 'nearest',
-                'midpoint')],
-            columns=['interpolation', 'min', 'q1', 'q2',
-                     'q3', 'max', 'iqr']).\
-            set_index(['interpolation'])
+    """
+    Return six statistics
+
+    Returns
+    -------
+    min            = minimum value
+    quantile(0.25) = first quartile
+    quantile(0.50) = median
+    quantile(0.75) = third quartile
+    max            = maximum value
+    iqr            = interquartile range
+    """
+    # Pourqoi pas juste:
+    #     five = five_number_summary(data)
+    #     five['iqr'] = five['q3'] - five['q1']
+    #     return five
+    return pd.DataFrame(
+        [(interpolation,
+          data.min(),
+          data.quantile(0.25, interpolation=interpolation),
+          data.quantile(0.50, interpolation=interpolation),
+          data.quantile(0.75, interpolation=interpolation),
+          data.max(),
+          (data.quantile(0.75, interpolation=interpolation) -
+           data.quantile(0.25, interpolation=interpolation)))
+         for interpolation
+         in ('linear', 'lower', 'higher', 'nearest', 'midpoint')],
+        columns=['interpolation', 'min', 'q1', 'q2', 'q3', 'max', 'iqr']
+    ).set_index(['interpolation'])
