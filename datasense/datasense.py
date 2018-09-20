eb2f168813c404f1cfd7a39af529834c4f388f7a
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-09-20 11:54:29 -0500

    Add seven-number summary function

--- datasense/datasense.py
+++ datasense/datasense.py
@@ -57,3 +57,38 @@ def six_number_summary(data: pd.Series) -> pd.DataFrame:
          in ('linear', 'lower', 'higher', 'nearest', 'midpoint')],
         columns=['interpolation', 'min', 'q1', 'q2', 'q3', 'max', 'iqr']
     ).set_index(['interpolation'])
+
+
+def seven_number_summary(data: pd.Series) -> pd.DataFrame:
+    """
+    Return six statistics
+
+    Returns
+    -------
+    min            = minimum value
+    quantile(0.25) = first quartile
+    quantile(0.50) = median
+    quantile(0.75) = third quartile
+    max            = maximum value
+    iqr            = interquartile range
+    n              = sample size
+    """
+    # Pourqoi pas juste:
+    #     five = five_number_summary(data)
+    #     five['iqr'] = five['q3'] - five['q1']
+    #     return five
+    return pd.DataFrame(
+        [(interpolation,
+          data.min(),
+          data.quantile(0.25, interpolation=interpolation),
+          data.quantile(0.50, interpolation=interpolation),
+          data.quantile(0.75, interpolation=interpolation),
+          data.max(),
+          (data.quantile(0.75, interpolation=interpolation) -
+           data.quantile(0.25, interpolation=interpolation)),
+          data.count())
+         for interpolation
+         in ('linear', 'lower', 'higher', 'nearest', 'midpoint')],
+        columns=['interpolation', 'min', 'q1', 'q2', 'q3', 'max', 'iqr', 'n']
+    ).set_index(['interpolation'])
+
