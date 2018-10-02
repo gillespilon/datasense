ae7c8de57a914799e8a189364f0f109af441eadd
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-10-01 21:25:10 -0500

    Calculate plus_two_sigma_x

--- datasense/datasense.py
+++ datasense/datasense.py
@@ -264,4 +264,6 @@ def control_chart_xmr(df: pd.Series, subgroup_size) -> pd.DataFrame:
     sigma_x = average_mr / d2
     # Calculate the X chart upper control limit.
     x_chart_ucl = average + 3 * sigma_x
+    # Calculate two Sigma(X) above the average.
+    plus_two_sigma_x = average + 2 * sigma_x
     return x_chart_ucl
