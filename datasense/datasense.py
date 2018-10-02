fca6fa355bd52788cf64f630a3ec3e9611c85e6f
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-10-02 15:26:29 -0500

    Remove top and right spines from moving range chart

--- datasense/datasense.py
+++ datasense/datasense.py
@@ -309,5 +309,8 @@ def control_chart_xmr(
              .abs())\
              .plot.line(legend=False, marker='o',
                         markersize=3, color=lines_c)
+    # Remove the top and right psines.
+    for spine in 'right', 'top':
+        ax.spines[spine].set_color('none')
     plt.show()
     return ax
