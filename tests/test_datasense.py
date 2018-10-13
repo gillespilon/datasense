c3e4a5eb2bf6a3c487207807575e2875b9637d64
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-10-13 13:35:36 -0500

    Add testing boilerplate

---
+++ tests/test_datasense.py
@@ -0,0 +1,30 @@
+def test_sommaire_cinq_numeros():
+    raise NotImplementedError()
+
+
+def test_five_number_summary():
+    raise NotImplementedError()
+
+
+def test_six_number_summary():
+    raise NotImplementedError()
+
+
+def test_seven_number_summary():
+    raise NotImplementedError()
+
+
+def test_nonparametric_summary():
+    raise NotImplementedError()
+
+
+def test_parametric_summary():
+    raise NotImplementedError()
+
+
+def test_control_chart_constants():
+    raise NotImplementedError()
+
+
+def test_control_chart_xmr():
+    raise NotImplementedError()
