59537cc8a821805bd316055b82a6dac92c412354
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-10-15 07:12:39 -0500

    Add introductory text and purpose

--- datasense/__init__.py
+++ datasense/__init__.py
@@ -1,5 +1,20 @@
 '''
-TODO
+datasense
+
+Statistical and graphical functions for:
+
+- Data science
+- Analysing process variation
+- Six Sigma methodology
+
+Why this?
+
+- Wanted functions to support process control charts.
+- Wanted functions to support measurement system analysis.
+- Develop a package that is open-source for those unable to buy software.
+- Wanted Python functions that are available in R and SAS.
+- Other packages have limited features for process control analysis.
+- Other packages are abandone or inadequately supported.
 '''
 
 import pandas as pd
