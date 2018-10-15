8a3d24cf75a4c91d040472c1b00517e3364ce60a
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-10-15 07:20:32 -0500

    Add eleven TODOs
    - TODO: Measurement system analysis (MSA) using control charts.
    - TODO: MSA range chart.
    - TODO: MSA range chart.
    - TODO: MSA average chart.
    - TODO: MSA parallelism chart.
    - TODO: MSA main effects chart (ANOME).
    - TODO: MSA mean ranges chart (ANOMR).
    - TODO: MSA test-retest error.
    - TODO: MSA probable error.
    - TODO: MSA intraclass correlation coefficient with operator bias.
    - TODO: MSA intraclass correlation coefficient without operator bias.

--- datasense/__init__.py
+++ datasense/__init__.py
@@ -17,6 +17,18 @@ Why this?
 - Other packages are abandone or inadequately supported.
 '''
 
+# TODO: Measurement system analysis (MSA) using control charts.
+# TODO: MSA range chart.
+# TODO: MSA range chart.
+# TODO: MSA average chart.
+# TODO: MSA parallelism chart.
+# TODO: MSA main effects chart (ANOME).
+# TODO: MSA mean ranges chart (ANOMR).
+# TODO: MSA test-retest error.
+# TODO: MSA probable error.
+# TODO: MSA intraclass correlation coefficient with operator bias.
+# TODO: MSA intraclass correlation coefficient without operator bias.
+
 import pandas as pd
 import numpy as np
 import matplotlib.cm as cm
