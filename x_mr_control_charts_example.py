e7d1dbc052ee784907d919ae9ce5188601c40dba
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-10-23 09:04:46 -0500

    Add code to save graphics as svg.

--- x_mr_control_charts_example.py
+++ x_mr_control_charts_example.py
@@ -21,8 +21,9 @@ ax1 = x.ax
 ax1.set_title('X control chart' + '\n' 'Subtitle')
 ax1.set_ylabel('Response (units)')
 ax1.set_xlabel('X axis label')
-plt.show()
-#plt.clf()
+ax1.figure.savefig('x.svg', format='svg') # Comment if you wish interactive
+# plt.show() # Uncomment if you wish interactive
+plt.clf() # Comment if you wish interactive
 mr = mR(chart_data)
 print('mR chart')
 print('Upper control limit', mr.ucl, sep=' = ')
@@ -35,5 +36,6 @@ ax2 = mr.ax
 ax2.set_title('mR control chart' + '\n' 'Subtitle')
 ax2.set_ylabel('Response (units)')
 ax2.set_xlabel('X axis label')
-plt.show()
-#plt.clf()
+ax2.figure.savefig('mr.svg', format='svg') # Comment if you wish interactive
+# plt.show() # Uncomment if you wish interactive
+plt.clf() # Comment if you wish interactive
