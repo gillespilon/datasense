743824235540ff628e6ff2815d284aef27302d6b
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-10-31 15:45:43 -0500

    Delete six_number_summary

--- datasense/__init__.py
+++ datasense/__init__.py
@@ -51,38 +51,6 @@ _QUANTILE_METHODS = [
 ]
 
 
-def six_number_summary(series: pd.Series) -> pd.DataFrame:
-    '''
-    Return six statistics
-
-    Returns
-    -------
-    min            = minimum value
-    quantile(0.25) = first quartile
-    quantile(0.50) = median
-    quantile(0.75) = third quartile
-    max            = maximum value
-    iqr            = interquartile range
-    '''
-    # Pourqoi pas juste:
-    #     five = five_number_summary(series)
-    #     five['iqr'] = five['q3'] - five['q1']
-    #     return five
-    return pd.DataFrame(
-        [(interpolation,
-          series.min(),
-          series.quantile(0.25, interpolation=interpolation),
-          series.quantile(0.50, interpolation=interpolation),
-          series.quantile(0.75, interpolation=interpolation),
-          series.max(),
-          (series.quantile(0.75, interpolation=interpolation) -
-           series.quantile(0.25, interpolation=interpolation)))
-         for interpolation
-         in ('linear', 'lower', 'higher', 'nearest', 'midpoint')],
-        columns=['interpolation', 'min', 'q1', 'q2', 'q3', 'max', 'iqr']
-    ).set_index(['interpolation'])
-
-
 def seven_number_summary(series: pd.Series) -> pd.DataFrame:
     '''
     Return six statistics
