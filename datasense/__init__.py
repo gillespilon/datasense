gillespilon/datasense

commit 79ea1261b63bb08dfae87dc914964b92ad3329bb

Replace manual rolling diff with pandas optimized one
Using a (Python) lambda is slower than just letting pandas do its thing
in C or whatever vector implementation it has. Come on! It shouldn't
take 15 seconds to just do a rolling difference of 75K items!

Reduces a performance test from ~2500 seconds to ~1000.

datasense/__init__.py
@@ -381,8 +381,7 @@ class ControlChart(ABC):
         assert subgroup_size >= 2
         return (
             self._df.iloc[:, 0]
-                    .rolling(subgroup_size)
-                    .agg(lambda x: x.iloc[0] - x.iloc[1])
+                    .diff(subgroup_size - 1)
                     .abs()
                     .mean()
         )
@@ -478,8 +477,7 @@ class mR(ControlChart):
     def ax(self) -> axes.Axes:
         'Matplotlib control chart plot'
         ax = (self._df
-                  .rolling(self.subgroup_size)
-                  .agg(lambda x: x.iloc[0] - x.iloc[1])
+                  .diff(periods=self.subgroup_size - 1)
                   .abs()
                   .plot.line(legend=False, marker='o',
                              markersize=3, color=_lines_c,))

commit d24887f33fad690c5098361ec8e1caf4e9bd5cae

Fix moving range for subgroup size > 2
It's the max_window - min_window, not start_window - end_window.

Fixes: 79ea126 (Replace manual rolling diff with pandas optimized one)

datasense/__init__.py
@@ -265,12 +265,11 @@ class ControlChart(ABC):
         if subgroup_size is None:
             subgroup_size = 2
         assert subgroup_size >= 2
+        _ = self._df.iloc[:, 0]
         return (
-            self._df.iloc[:, 0]
-                    .diff(subgroup_size - 1)
-                    .abs()
-                    .mean()
-        )
+            _.rolling(subgroup_size).max() -
+            _.rolling(subgroup_size).min()
+        ).mean()
 
 
 # Use a colour-blind friendly colormap, 'Paired'.
@@ -362,11 +361,11 @@ class mR(ControlChart):
     @cached_property
     def ax(self) -> axes.Axes:
         'Matplotlib control chart plot'
-        ax = (self._df
-                  .diff(periods=self.subgroup_size - 1)
-                  .abs()
-                  .plot.line(legend=False, marker='o',
-                             markersize=3, color=_lines_c,))
+        ax = (
+            self._df.rolling(self.subgroup_size).max() -
+            self._df.rolling(self.subgroup_size).min()
+        ).plot.line(legend=False, marker='o',
+                    markersize=3, color=_lines_c,)
         # TODO? ax.set_xlim(0, len(self._df.columns))
         _despine(ax)
         ax.axhline(y=self.mean, color=_average_c)