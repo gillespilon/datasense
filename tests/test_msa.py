c0c29719552c520310f3339b3fc38d23d17b4eb5
Gilles Pilon <gillespilon13@gmail.com>
2019-08-02 10:08:34 -0500

    Initial commit test_msa.py

---
+++ tests/test_msa.py
@@ -0,0 +1,81 @@
+import datasense.msa as msa
+
+
+def test_range_chart():
+    raise NotImplementedError()
+
+
+def test_average_chart():
+    raise NotImplementedError()
+
+
+def test_parallelism_plot():
+    raise NotImplementedError()
+
+
+def test_analysis_of_main_effects():
+    raise NotImplementedError()
+
+
+def test_analysis_of_mean_ranges():
+    raise NotImplementedError()
+
+
+def test_emp_results():
+    raise NotImplementedError()
+
+
+def test_classification():
+    raise NotImplementedError()
+
+
+def test_effective_resolution():
+    raise NotImplementedError()
+
+
+def test_variance_components():
+    raise NotImplementedError()
+
+
+def test_emp_gauge_rr_results():
+    raise NotImplementedError()
+
+
+def test_interpret():
+    raise NotImplementedError()
+
+
+def test_interpret_tables():
+    raise NotImplementedError()
+
+
+def test_range_in_control():
+    raise NotImplementedError()
+
+
+def test_range_out_of_control_reason():
+    raise NotImplementedError()
+
+
+def test_average_in_control():
+    raise NotImplementedError()
+
+
+def test_average_out_of_control_reason():
+    raise NotImplementedError()
+
+
+def test_main_effects_in_control():
+    raise NotImplementedError()
+
+
+def test_main_effects_out_of_control_reason():
+    raise NotImplementedError()
+
+
+def test_mean_ranges_in_control():
+    raise NotImplementedError()
+
+
+def test_mean_ranges_out_of_control_reason():
+    raise NotImplementedError()
