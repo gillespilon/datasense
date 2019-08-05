9bc4eba5a42c79e321aee7afac2762da4048c8fc
Gilles Pilon <gillespilon13@gmail.com>
2019-08-05 14:25:18 -0500

    Add test for calling functions

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,9 +1,15 @@
 import datasense.stats as st
+import pandas as pd
+
+
+myseries = pd.Series([1, 3, 6])
 
 
 def test_nonparametric_summary():
-    raise NotImplementedError()
+    st.nonparametric_summary(myseries)
+    return
 
 
 def test_parametric_summary():
-    raise NotImplementedError()
+    st.parametric_summary(myseries)
+    return
