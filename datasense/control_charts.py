gillespilon/datasense

commit bb089b4827923efa98fbd72151074e665fa0609d

Add points_three, _fivewise

datasense/control_charts.py
@@ -246,6 +246,9 @@ class X(ControlChart):
         ax.axhline(y=self.mean, color=c[2])
         ax.axhline(y=self.ucl, color=c[1])
         ax.axhline(y=self.lcl, color=c[1])
+        # next two lines temporary, to test points_three()
+        ax.axhline(y=(self.mean + self.sigma), color=c[3])
+        ax.axhline(y=(self.mean - self.sigma), color=c[3])
 
         return ax
 
@@ -502,6 +505,21 @@ def _threewise(it):
     return zip(a, b, c)
 
 
+def _fivewise(it):
+    a, b, c, d, e = tee(it, 5)
+    next(b, None)
+    next(c, None)
+    next(c, None)
+    next(d, None)
+    next(d, None)
+    next(d, None)
+    next(e, None)
+    next(e, None)
+    next(e, None)
+    next(e, None)
+    return zip(a, b, c, d, e)
+
+
 def points_one(cc: ControlChart) -> Tuple[pd.Series, pd.Series]:
     '''
     Shewhart and Western Electric Rule one.
@@ -547,7 +565,22 @@ def points_three(cc: ControlChart) -> Tuple[pd.Series, pd.Series]:
     and are more than one sigma units away from the central line.
     This rule is used with the X and Xbar charts.
     '''
-    pass
+    above = []
+    below = []
+    for group in _fivewise(cc.y.items()):
+        above_in_window = [(x, y)
+                           for x, y
+                           in group
+                           if y > cc.sigmas[+1]]
+        if len(above_in_window) >= 4:
+            above.append(above_in_window[-1])
+        below_in_window = [(x, y)
+                           for x, y
+                           in group
+                           if y < cc.sigmas[-1]]
+        if len(below_in_window) >= 4:
+            below.append(below_in_window[-1])
+    return pd.Series(dict(above)), pd.Series(dict(below))
 
 
 def points_four(cc: ControlChart) -> Tuple[pd.Series, pd.Series]:

commit f167fc16f4ebfb50f2094a91658d5bedfca02e8b

Mark 2nd and 4th incorrect points in rules 2 and 3, not last
If 3/3 points in rule 2 are wrong, mark the 2nd, not the 3rd. Same with
rule 3.

Fixes: bb089b4 (Add points_three, _fivewise)
Fixes: 5669804 (Add Shewhart control chart rule two)

datasense/control_charts.py
@@ -544,13 +544,13 @@ def points_two(cc: ControlChart) -> Tuple[pd.Series, pd.Series]:
                            in group
                            if y > cc.sigmas[+2]]
         if len(above_in_window) >= 2:
-            above.append(above_in_window[-1])
+            above.append(above_in_window[1])  # whether 2 or 3, 2nd
         below_in_window = [(x, y)
                            for x, y
                            in group
                            if y < cc.sigmas[-2]]
         if len(below_in_window) >= 2:
-            below.append(below_in_window[-1])
+            below.append(below_in_window[1])  # whether 2 or 3, 2nd
     return pd.Series(dict(above)), pd.Series(dict(below))
 
 
@@ -570,13 +570,13 @@ def points_three(cc: ControlChart) -> Tuple[pd.Series, pd.Series]:
                            in group
                            if y > cc.sigmas[+1]]
         if len(above_in_window) >= 4:
-            above.append(above_in_window[-1])
+            above.append(above_in_window[3])  # whether 4 or 5, 4th point
         below_in_window = [(x, y)
                            for x, y
                            in group
                            if y < cc.sigmas[-1]]
         if len(below_in_window) >= 4:
-            below.append(below_in_window[-1])
+            below.append(below_in_window[3])  # whether 4 or 5, 4th point
     return pd.Series(dict(above)), pd.Series(dict(below))
 
 