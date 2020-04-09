4a061d2e5b1bb7077c4719ec8a7e75cc85bb6472
Gilles Pilon <gillespilon13@gmail.com>
2020-04-09 16:55:14 -0500

    Initial commit graphs.py

---
+++ datasense/graphs.py
@@ -0,0 +1,17 @@
+'''
+Graphical analysis
+'''
+
+
+def plot_line_x_y1_y2():
+    '''
+    Line plot of y1 left vertical axis versus x
+    Line plot of y2 right vertical axis versus x
+    Smoothing applied to y1, y2
+    '''
+    pass
+
+
+__all__ = (
+    'plot_line_x_y1_y2'
+)
