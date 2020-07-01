ccd6baeff3af6d6c2816a75512dd6e69715e6f11
Gilles Pilon <gillespilon13@gmail.com>
2020-07-01 18:35:59 -0500

    Added smoothing to plot_line_line_x_y1_y2()

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -186,46 +186,22 @@ def plot_line_line_x_y1_y2(
     '''
     fig = plt.figure(figsize=figuresize)
     ax = fig.add_subplot(111)
-    if X.dtype in ['datetime64[ns]']:
-        fig.autofmt_xdate()
-    ax.plot(X, y1, marker=None, linestyle='-', color=c[1])
-    ax.plot(X, y2, marker=None, linestyle='-', color=c[5])
+    if smoothing is None:
+        if X.dtype in ['datetime64[ns]']:
+            fig.autofmt_xdate()
+        ax.plot(X, y1, marker=None, linestyle='-', color=c[1])
+        ax.plot(X, y2, marker=None, linestyle='-', color=c[5])
+    elif smoothing == 'natural_cubic_spline':
+        if X.dtype in ['datetime64[ns]']:
+            XX = pd.to_numeric(X)
+            fig.autofmt_xdate()
+        else:
+            XX = X
+        model1 = natural_cubic_spline(XX, y1, numknots)
+        model2 = natural_cubic_spline(XX, y2, numknots)
+        ax.plot(X, model1.predict(XX), marker=None, linestyle='-', color=c[1])
+        ax.plot(X, model2.predict(XX), marker=None, linestyle='-', color=c[5])
     return ax
-
-
-def plot_lineleft_lineright_x_y1_y2(
-    X: pd.Series,
-    y1: pd.Series,
-    y2: pd.Series,
-    figuresize: Optional[plt.Figure] = None,
-    smoothing: str = None
-) -> Tuple[axes.Axes]:
-    '''
-    Line plot of y1 left vertical axis versus X
-    Line plot of y2 right vertical axis versus X
-    Optional smoothing applied to y1, y2
-
-    This graph is useful if y1 and y2 have different units or scales,
-    and you wish to see if they are correlated.
-
-    x:  series for horizontal axis
-    y1: series for y1 to plot using left vertical axis
-    y2: series for y2 to plot using right vertical axis
-
-    If smoothing is applied, the series must not contain NaN, inf, or -inf
-    '''
-    fig = plt.figure(figsize=figuresize)
-    ax1 = fig.add_subplot(111)
-    ax2 = ax1.twinx()
-    if X.dtype in ['datetime64[ns]']:
-        fig.autofmt_xdate()
-    ax1.plot(X, y1, color=c[1])
-    ax2.plot(X, y2, color=c[5])
-    for tl in ax1.get_yticklabels():
-        tl.set_color(c[1])
-    for tl in ax2.get_yticklabels():
-        tl.set_color(c[5])
-    return (ax1, ax2)
 
 
 def plot_scatterleft_scatterright_x_y1_y2(
@@ -233,7 +209,8 @@ def plot_scatterleft_scatterright_x_y1_y2(
     y1: pd.Series,
     y2: pd.Series,
     figuresize: Optional[plt.Figure] = None,
-    smoothing: str = None
+    smoothing: str = None,
+    numknots: int = None
 ) -> Tuple[axes.Axes]:
     '''
     Scatter plot of y1 left vertical axis versus X
@@ -256,6 +233,42 @@ def plot_scatterleft_scatterright_x_y1_y2(
         fig.autofmt_xdate()
     ax1.plot(X, y1, marker='.', linestyle='', color=c[1])
     ax2.plot(X, y2, marker='.', linestyle='', color=c[5])
+    for tl in ax1.get_yticklabels():
+        tl.set_color(c[1])
+    for tl in ax2.get_yticklabels():
+        tl.set_color(c[5])
+    return (ax1, ax2)
+
+
+def plot_lineleft_lineright_x_y1_y2(
+    X: pd.Series,
+    y1: pd.Series,
+    y2: pd.Series,
+    figuresize: Optional[plt.Figure] = None,
+    smoothing: str = None,
+    numknots: int = None
+) -> Tuple[axes.Axes]:
+    '''
+    Line plot of y1 left vertical axis versus X
+    Line plot of y2 right vertical axis versus X
+    Optional smoothing applied to y1, y2
+
+    This graph is useful if y1 and y2 have different units or scales,
+    and you wish to see if they are correlated.
+
+    x:  series for horizontal axis
+    y1: series for y1 to plot using left vertical axis
+    y2: series for y2 to plot using right vertical axis
+
+    If smoothing is applied, the series must not contain NaN, inf, or -inf
+    '''
+    fig = plt.figure(figsize=figuresize)
+    ax1 = fig.add_subplot(111)
+    ax2 = ax1.twinx()
+    if X.dtype in ['datetime64[ns]']:
+        fig.autofmt_xdate()
+    ax1.plot(X, y1, color=c[1])
+    ax2.plot(X, y2, color=c[5])
     for tl in ax1.get_yticklabels():
         tl.set_color(c[1])
     for tl in ax2.get_yticklabels():
