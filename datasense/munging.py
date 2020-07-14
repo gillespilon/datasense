d8fd0023be2ca0543eec3c9cecc2be7cc7303d81
Gilles Pilon <gillespilon13@gmail.com>
2020-07-14 12:14:17 -0500

    dded text-wrapping for columns_float_list

--- datasense/munging.py
+++ datasense/munging.py
@@ -59,8 +59,11 @@ def dataframe_info(df: pd.DataFrame, filein: str) -> pd.DataFrame:
     for element in new_list:
         print(element)
     print()
-    print(f'List of {columns_float_count} float columns:\n'
-          f'{columns_float_list}')
+    print(f'List of {columns_float_count} float columns:')
+    string_not_list = ", ".join(columns_float_list)
+    new_list = wrapper.wrap(string_not_list)
+    for element in new_list:
+        print(element)
     print()
     print(f'List of {columns_integer_count} integer columns:\n'
           f'{columns_integer_list}')
