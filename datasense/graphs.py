f660d0b76bc242bda619c35d20cb7d3c877424e8
Gilles Pilon <gillespilon13@gmail.com>
2020-08-02 12:32:58 -0500

    Add plot_scatter_y()

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -2,7 +2,6 @@
 Graphical analysis
 '''
 
-
 from typing import Optional, Tuple
 
 from datasense import natural_cubic_spline
@@ -16,6 +15,32 @@ import pandas as pd
 c = cm.Paired.colors
 
 
+def plot_scatter_y(
+    y: pd.Series,
+    figuresize: Optional[plt.Figure] = None,
+    smoothing: str = None,
+    numknots: int = None
+) -> (plt.figure, axes.Axes):
+    '''
+    Scatter plot of y.
+    Optional smoothing applied to y.
+
+    y: series for vertical axis
+
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
+    '''
+
+    fig = plt.figure(figsize=figuresize)
+    ax = fig.add_subplot(111)
+    X = pd.Series(range(1, y.size + 1, 1))
+    if smoothing is None:
+        ax.plot(y, marker='.', linestyle='', color=c[1])
+    elif smoothing == 'natural_cubic_spline':
+        model = natural_cubic_spline(X, y, numknots)
+        ax.plot(X, model.predict(X), marker='.', linestyle='', color=c[1])
+    return (fig, ax)
+
+
 def plot_scatter_x_y(
     X: pd.Series,
     y: pd.Series,
@@ -24,13 +49,12 @@ def plot_scatter_x_y(
     numknots: int = None
 ) -> (plt.figure, axes.Axes):
     '''
-    Scatter plot of y versus X
-    Optional smoothing applied to y
+    Scatter plot of y versus X.  Optional smoothing applied to y.
 
     X: series for horizontal axis
     y: series for vertical axis
 
-    If smoothing is applied, the series must not contain NaN, inf, or -inf
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
     '''
 
     fig = plt.figure(figsize=figuresize)
@@ -58,13 +82,13 @@ def plot_line_x_y(
     numknots: int = None
 ) -> (plt.figure, axes.Axes):
     '''
-    Scatter plot of y versus X
-    Optional smoothing applied to y
+    Scatter plot of y versus X.
+    Optional smoothing applied to y.
 
     X: series for horizontal axis
     y: series for vertical axis
 
-    If smoothing is applied, the series must not contain NaN, inf, or -inf
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
     '''
 
     fig = plt.figure(figsize=figuresize)
@@ -93,9 +117,9 @@ def plot_scatter_scatter_x_y1_y2(
     numknots: int = None
 ) -> axes.Axes:
     '''
-    Scatter plot of y1 versus X
-    Scatter plot of y2 versus X
-    Optional smoothing applied to y1, y2
+    Scatter plot of y1 versus X.
+    Scatter plot of y2 versus X.
+    Optional smoothing applied to y1, y2.
 
     This graph is useful if y1 and y2 have the same units.
 
@@ -103,7 +127,7 @@ def plot_scatter_scatter_x_y1_y2(
     y1: series for y1 to plot on vertical axis
     y2: series for y2 to plot on vertical axis
 
-    If smoothing is applied, the series must not contain NaN, inf, or -inf
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
     '''
 
     fig = plt.figure(figsize=figuresize)
@@ -135,9 +159,9 @@ def plot_scatter_line_x_y1_y2(
     numknots: int = None
 ) -> axes.Axes:
     '''
-    Scatter plot of y1 versus X
-    Line plot of y2 versus X
-    Optional smoothing applied to y1, y2
+    Scatter plot of y1 versus X.
+    Line plot of y2 versus X.
+    Optional smoothing applied to y1, y2.
 
     This graph is useful if y1 and y2 have the same units.
 
@@ -145,7 +169,7 @@ def plot_scatter_line_x_y1_y2(
     y1: series for y1 to plot on vertical axis
     y2: series for y2 to plot on vertical axis
 
-    If smoothing is applied, the series must not contain NaN, inf, or -inf
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
     '''
 
     fig = plt.figure(figsize=figuresize)
@@ -177,9 +201,9 @@ def plot_line_line_x_y1_y2(
     numknots: int = None
 ) -> axes.Axes:
     '''
-    Line plot of y1 versus X
-    Line plot of y2 versus X
-    Optional smoothing applied to y1, y2
+    Line plot of y1 versus X.
+    Line plot of y2 versus X.
+    Optional smoothing applied to y1, y2.
 
     This graph is useful if y1 and y2 have the same units.
 
@@ -187,7 +211,7 @@ def plot_line_line_x_y1_y2(
     y1: series for y1 to plot on vertical axis
     y2: series for y2 to plot on vertical axis
 
-    If smoothing is applied, the series must not contain NaN, inf, or -inf
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
     '''
 
     fig = plt.figure(figsize=figuresize)
@@ -219,9 +243,9 @@ def plot_scatterleft_scatterright_x_y1_y2(
     numknots: int = None
 ) -> Tuple[axes.Axes]:
     '''
-    Scatter plot of y1 left vertical axis versus X
-    Scatter plot of y2 right vertical axis versus X
-    Optional smoothing applied to y1, y2
+    Scatter plot of y1 left vertical axis versus X.
+    Scatter plot of y2 right vertical axis versus X.
+    Optional smoothing applied to y1, y2.
 
     This graph is useful if y1 and y2 have different units or scales,
     and you wish to see if they are correlated.
@@ -230,7 +254,7 @@ def plot_scatterleft_scatterright_x_y1_y2(
     y1: series for y1 to plot using left vertical axis
     y2: series for y2 to plot using right vertical axis
 
-    If smoothing is applied, the series must not contain NaN, inf, or -inf
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
     '''
 
     fig = plt.figure(figsize=figuresize)
@@ -267,9 +291,9 @@ def plot_lineleft_lineright_x_y1_y2(
     numknots: int = None
 ) -> Tuple[axes.Axes]:
     '''
-    Line plot of y1 left vertical axis versus X
-    Line plot of y2 right vertical axis versus X
-    Optional smoothing applied to y1, y2
+    Line plot of y1 left vertical axis versus X.
+    Line plot of y2 right vertical axis versus X.
+    Optional smoothing applied to y1, y2.
 
     This graph is useful if y1 and y2 have different units or scales,
     and you wish to see if they are correlated.
@@ -278,7 +302,7 @@ def plot_lineleft_lineright_x_y1_y2(
     y1: series for y1 to plot using left vertical axis
     y2: series for y2 to plot using right vertical axis
 
-    If smoothing is applied, the series must not contain NaN, inf, or -inf
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
     '''
 
     fig = plt.figure(figsize=figuresize)
@@ -322,6 +346,7 @@ def format_dates(
 
 
 __all__ = (
+    'plot_scatter_y',
     'plot_scatter_x_y',
     'plot_line_x_y',
     'plot_scatter_scatter_x_y1_y2',
