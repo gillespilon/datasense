0ffa52db61ed5b9bd83a3e78063f819715a11ac5
Gilles Pilon <gillespilon13@gmail.com>
2020-09-14 09:51:10 -0500

    Add two examples to test

--- scripts/plot_scatter_x_y_test.py
+++ scripts/plot_scatter_x_y_test.py
@@ -6,7 +6,10 @@ time -f '%e' ./plot_scatter_x_y_test.py
 ./plot_scatter_x_y_test.py
 '''
 
+from numpy.random import default_rng
+import matplotlib.pyplot as plt
 import datasense as ds
+import pandas as pd
 
 
 def main():
@@ -27,6 +30,7 @@ def main():
         colour='#ee7733'
     )
     fig.savefig('plot_scatter_x_y_datex_test.svg', format='svg')
+
     fig, ax = ds.plot_scatter_x_y(
         df['BOARD_MIXER_MANIFOLD_PRESS'],
         df['BOARD_MIXER_KW'],
@@ -36,6 +40,40 @@ def main():
         colour='#ee7733'
     )
     fig.savefig('plot_scatter_x_y_floatx_test.svg', format='svg')
+
+    rng = default_rng()
+    data_x = rng.uniform(
+        low=13,
+        high=69,
+        size=42
+    )
+    series_x = pd.Series(data_x)
+    data_y = rng.standard_normal(size=42)
+    series_y = pd.Series(data_y)
+    fig, ax = ds.plot_scatter_x_y(
+        X=series_x,
+        y=series_y
+    )
+    fig.savefig('plot_scatter_x_y_test_1.svg', format='svg')
+
+    rng = default_rng()
+    data_x = rng.uniform(
+        low=13,
+        high=69,
+        size=42
+    )
+    series_x = pd.Series(data_x)
+    data_y = rng.standard_normal(size=42)
+    series_y = pd.Series(data_y)
+    fig, ax = ds.plot_scatter_x_y(
+        X=series_x,
+        y=series_y,
+        figuresize=(8, 6),
+        marker='o',
+        markersize=8,
+        colour='#cc3311'
+    )
+    fig.savefig('plot_scatter_x_y_test_2.svg', format='svg')
 
 
 if __name__ == '__main__':
