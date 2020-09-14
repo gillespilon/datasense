4ec8f644888107b95156b16fc04a18af38c934fc
Gilles Pilon <gillespilon13@gmail.com>
2020-09-14 08:55:34 -0500

    Add example 2, refactor example 1

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -65,16 +65,30 @@ def plot_scatter_y(
 
     Example
     ------
-        import matplotlib.pyplot as plt
-        from numpy.random import default_rng
-        import datasense as ds
-        import pandas as pd
+    Example 1
+        >>> import matplotlib.pyplot as plt
+        >>> from numpy.random import default_rng
+        >>> import datasense as ds
+        >>> import pandas as pd
+        >>>
+        >>> rng = default_rng()
+        >>> data = rng.standard_normal(size=42)
+        >>> series = pd.Series(data)
+        >>> fig, ax = ds.plot_scatter_y(y=series)
+        >>> plt.show()
 
-        rng = default_rng()
-        data = rng.standard_normal(size=42)
-        series = pd.Series(data)
-        fig, ax = ds.plot_scatter_y(y=series)
-        plt.show()
+    Example 2
+        >>> rng = default_rng()
+        >>> data = rng.standard_normal(size=42)
+        >>> series = pd.Series(data)
+        >>> fig, ax = ds.plot_scatter_y(
+        >>>     y=series,
+        >>>     figuresize=(8, 6),
+        >>>     marker='o',
+        >>>     markersize=8,
+        >>>     colour='#cc3311'
+        >>> )
+        >>> plt.show()
     '''
     fig = plt.figure(figsize=figuresize)
     ax = fig.add_subplot(111)
