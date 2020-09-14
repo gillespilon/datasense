976519dd7bc871b2444bfbf5fa62305206845428
Gilles Pilon <gillespilon13@gmail.com>
2020-09-14 09:55:19 -0500

    Add two examples to test

--- scripts/plot_scatter_y_test.py
+++ scripts/plot_scatter_y_test.py
@@ -6,9 +6,10 @@ time -f '%e' ./plot_scatter_y_test.py
 ./plot_scatter_y_test.py
 '''
 
+from numpy.random import default_rng
+import matplotlib.pyplot as plt
 import datasense as ds
 import pandas as pd
-from numpy.random import default_rng
 
 
 def main():
@@ -28,6 +29,13 @@ def main():
         colour='#ee7733'
     )
     fig.savefig('plot_scatter_y_test_1.svg', format='svg')
+
+    rng = default_rng()
+    data = rng.standard_normal(size=42)
+    series = pd.Series(data)
+    fig, ax = ds.plot_scatter_y(y=series)
+    fig.savefig('plot_scatter_y_test_2.svg', format='svg')
+
     rng = default_rng()
     data = rng.standard_normal(size=42)
     series = pd.Series(data)
@@ -38,7 +46,12 @@ def main():
         markersize=4,
         colour='#ee7733'
     )
-    fig.savefig('plot_scatter_y_test_2.svg', format='svg')
+    fig.savefig('plot_scatter_y_test_3.svg', format='svg')
+
+rng = default_rng()
+data = rng.standard_normal(size=42)
+series = pd.Series(data)
+fig, ax = ds.plot_scatter_y(y=series)
 
 
 if __name__ == '__main__':
