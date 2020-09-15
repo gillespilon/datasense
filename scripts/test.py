ae98d8069afb8f4570ce39050f2cc23f462ddec1
Gilles Pilon <gillespilon13@gmail.com>
2020-09-15 14:03:51 -0500

    Edit for latest test of graphs.py

--- scripts/test.py
+++ scripts/test.py
@@ -5,34 +5,13 @@ import datasense as ds
 import pandas as pd
 
 rng = default_rng()
-data_x = rng.uniform(
-    low=13,
-    high=69,
-    size=42
-)
-series_x = pd.Series(data_x)
-data_y = rng.standard_normal(size=42)
-series_y = pd.Series(data_y)
-fig, ax = ds.plot_scatter_x_y(
-    X=series_x,
-    y=series_y
-)
+data = rng.standard_normal(size=42)
+series = pd.Series(data)
+fig, ax = ds.plot_scatter_y(y=series)
 plt.show()
 
-data_x = rng.uniform(
-    low=13,
-    high=69,
-    size=42
-)
-series_x = pd.Series(data_x)
-data_y = rng.standard_normal(size=42)
-series_y = pd.Series(data_y)
-fig, ax = ds.plot_scatter_x_y(
-    X=series_x,
-    y=series_y,
-    figuresize=(8, 6),
-    marker='o',
-    markersize=8,
-    colour='#cc3311'
-)
+rng = default_rng()
+data = rng.standard_normal(size=42)
+series = pd.Series(data)
+fig, ax = ds.plot_scatter_y(y=series)
 plt.show()
