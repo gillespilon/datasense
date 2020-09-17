b2eed3c979cca8a23e9ea23d0ea72390d29367fc
Gilles Pilon <gillespilon13@gmail.com>
2020-09-17 12:14:19 -0500

    Create data routines, refactor code

--- scripts/plot_scatter_x_y_test.py
+++ scripts/plot_scatter_x_y_test.py
@@ -7,8 +7,10 @@ time -f '%e' ./plot_scatter_x_y_test.py
 '''
 
 from numpy.random import default_rng
+from datetime import timedelta
+from pandas import Series
+from numpy import arange
 import datasense as ds
-import pandas as pd
 
 output_url = 'plot_scatter_x_y_test.html'
 header_title = 'plot_scatter_x_y_test'
@@ -21,17 +23,19 @@ def main():
         headertitle=header_title,
         headerid=header_id
     )
-    df = ds.read_file(
-        filename='norfolk.csv',
-        abscissa='LAB_BOARD_DAT_COD',
-        datetimeparser='%d%b%Y:%H:%M:%S'
+    rng = default_rng()
+    series_x = Series(
+        arange(
+            '2020-01-01T13:13:13',
+            '2020-02-12T13:13:13',
+            timedelta(hours=24),
+            dtype='datetime64[s]'
+        )
     )
-#     df = df.iloc[601:700, :]
-    print(df.head())
-    print(df.dtypes)
+    series_y = Series(rng.standard_normal(size=42))
     fig, ax = ds.plot_scatter_x_y(
-        df['LAB_BOARD_DAT_COD'],
-        df['BOARD_MIXER_KW'],
+        X=series_x,
+        y=series_y,
         figuresize=(8, 6),
         marker='1',
         markersize=4,
@@ -47,10 +51,16 @@ def main():
         '</figure>'
         '</p>'
     )
-
+    series_x = Series(
+        rng.uniform(
+            low=13,
+            high=69,
+            size=42
+        )
+    )
     fig, ax = ds.plot_scatter_x_y(
-        df['BOARD_MIXER_MANIFOLD_PRESS'],
-        df['BOARD_MIXER_KW'],
+        X=series_x,
+        y=series_y,
         figuresize=(8, 6),
         marker='1',
         markersize=4,
@@ -67,15 +77,6 @@ def main():
         '</p>'
     )
 
-    rng = default_rng()
-    data_x = rng.uniform(
-        low=13,
-        high=69,
-        size=42
-    )
-    series_x = pd.Series(data_x)
-    data_y = rng.standard_normal(size=42)
-    series_y = pd.Series(data_y)
     fig, ax = ds.plot_scatter_x_y(
         X=series_x,
         y=series_y
@@ -89,15 +90,12 @@ def main():
         '</figure>'
         '</p>'
     )
-
     data_x = rng.uniform(
         low=13,
         high=69,
         size=42
     )
-    series_x = pd.Series(data_x)
-    data_y = rng.standard_normal(size=42)
-    series_y = pd.Series(data_y)
+    series_x = Series(data_x)
     fig, ax = ds.plot_scatter_x_y(
         X=series_x,
         y=series_y,
