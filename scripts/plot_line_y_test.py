c44ed7416c761ecb860cf01cd4ce567553051583
Gilles Pilon <gillespilon13@gmail.com>
2020-09-18 14:13:54 -0500

    Refactor, simplify examples, use ds.html_figure

--- scripts/plot_line_y_test.py
+++ scripts/plot_line_y_test.py
@@ -22,63 +22,20 @@ def main():
         headertitle=header_title,
         headerid=header_id
     )
-    df = ds.read_file(
-        filename='norfolk.csv',
-        abscissa='LAB_BOARD_DAT_COD',
-        datetimeparser='%d%b%Y:%H:%M:%S'
-    )
-    df = df.iloc[601:700, :]
-    print(df.head())
-    print(df.dtypes)
-    fig, ax = ds.plot_line_y(
-        y=df['BOARD_MIXER_KW'],
-        figuresize=(8, 6),
-        marker='1',
-        markersize=4,
-        colour='#ee7733'
-    )
-    fig.savefig('plot_line_y_test_1.svg', format='svg')
-    print(
-        '<p>'
-        '<figure>'
-        '<img src="plot_line_y_test_1.svg" alt="alternate text graph 1"/>'
-        '<figcaption>plot_line_y_test_1</figcaption>'
-        '</figure>'
-        '</p>'
-    )
-
     rng = default_rng()
-    data = rng.standard_normal(size=42)
-    series = pd.Series(data)
-    fig, ax = ds.plot_line_y(y=series)
-    fig.savefig('plot_line_y_test_2.svg', format='svg')
-    print(
-        '<p>'
-        '<figure>'
-        '<img src="plot_line_y_test_2.svg" alt="alternate text graph 1"/>'
-        '<figcaption>plot_line_y_test_2</figcaption>'
-        '</figure>'
-        '</p>'
-    )
-
-    data = rng.standard_normal(size=42)
-    series = pd.Series(data)
+    series_y = pd.Series(rng.standard_normal(size=42))
+    fig, ax = ds.plot_line_y(y=series_y)
+    fig.savefig('plot_line_y_test_1.svg', format='svg')
+    ds.html_figure(filename='plot_line_y_test_1.svg')
     fig, ax = ds.plot_line_y(
-        y=series,
-        figuresize=(8, 6),
+        y=series_y,
+        figuresize=(8, 4.5),
         marker='o',
         markersize=4,
         colour='#ee7733'
     )
-    fig.savefig('plot_line_y_test_3.svg', format='svg')
-    print(
-        '<p>'
-        '<figure>'
-        '<img src="plot_line_y_test_3.svg" alt="alternate text graph 1"/>'
-        '<figcaption>plot_line_y_test_3</figcaption>'
-        '</figure>'
-        '</p>'
-    )
+    fig.savefig('plot_line_y_test_2.svg', format='svg')
+    ds.html_figure(filename='plot_line_y_test_2.svg')
     ds.html_end(
         originalstdout=original_stdout,
         outputurl=output_url
