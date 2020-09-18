3e4137560534ee5e775d1f150d55151067df82f7
Gilles Pilon <gillespilon13@gmail.com>
2020-09-18 15:08:48 -0500

    Refactor, simplify examples, use ds.html_figure

--- scripts/plot_line_x_y_test.py
+++ scripts/plot_line_x_y_test.py
@@ -7,6 +7,8 @@ time -f '%e' ./plot_line_x_y_test.py
 '''
 
 from numpy.random import default_rng
+from datetime import timedelta
+from numpy import arange
 import datasense as ds
 import pandas as pd
 
@@ -21,87 +23,40 @@ def main():
         headertitle=header_title,
         headerid=header_id
     )
-    df = ds.read_file(
-        filename='norfolk.csv',
-        abscissa='LAB_BOARD_DAT_COD',
-        datetimeparser='%d%b%Y:%H:%M:%S'
-    )
-#     df = df.iloc[601:700, :]
-    print(df.head())
-    print(df.dtypes)
-    fig, ax = ds.plot_line_x_y(
-        df['LAB_BOARD_DAT_COD'],
-        df['BOARD_MIXER_KW'],
-        figuresize=(8, 6),
-        markersize=2,
-        colour='#ee7733'
-    )
-    fig.savefig('plot_line_x_y_datex_test.svg', format='svg')
-    print(
-        '<p>'
-        '<figure>'
-        '<img src="plot_line_x_y_datex_test.svg" '
-        'alt="alternate text graph 1"/>'
-        '<figcaption>plot_line_x_y_datex_test</figcaption>'
-        '</figure>'
-        '</p>'
-    )
-
-    fig, ax = ds.plot_line_x_y(
-        pd.Series(range(1, df['LAB_BOARD_DAT_COD'].count() + 1)),
-        df['BOARD_MIXER_KW'],
-        figuresize=(8, 6),
-        markersize=2,
-        colour='#ee7733'
-    )
-    fig.savefig('plot_line_x_y_floatx_test.svg', format='svg')
-    print(
-        '<p>'
-        '<figure>'
-        '<img src="plot_line_x_y_floatx_test.svg" '
-        'alt="alternate text graph 1"/>'
-        '<figcaption>plot_line_x_y_floatx_test</figcaption>'
-        '</figure>'
-        '</p>'
-    )
-
     rng = default_rng()
-    series_x = pd.Series(range(1, 43))
+    series_x = pd.Series(
+        arange(
+            '2020-01-01T13:13:13',
+            '2020-02-12T13:13:13',
+            timedelta(hours=24),
+            dtype='datetime64[s]',
+        )
+    )
     series_y = pd.Series(rng.standard_normal(size=42))
     fig, ax = ds.plot_line_x_y(
         X=series_x,
         y=series_y
     )
-    fig.savefig('plot_line_x_y_test_1.svg', format='svg')
-    print(
-        '<p>'
-        '<figure>'
-        '<img src="plot_line_x_y_test_1.svg" alt="alternate text graph 1"/>'
-        '<figcaption>plot_line_x_y_test_1</figcaption>'
-        '</figure>'
-        '</p>'
-    )
-
-    series_x = pd.Series(range(1, 43))
-    series_y = pd.Series(rng.standard_normal(size=42))
+    fig.savefig('plot_line_x_y_datex_test.svg', format='svg')
+    ds.html_figure(filename='plot_line_x_y_datex_test.svg')
+    series_x = pd.Series(
+        rng.uniform(
+            low=13,
+            high=69,
+            size=42
+        )
+    ).sort_values()
     fig, ax = ds.plot_line_x_y(
         X=series_x,
         y=series_y,
-        figuresize=(8, 6),
+        figuresize=(8, 4.5),
         marker='o',
         markersize=8,
         linestyle=':',
-        colour='#cc3311'
+        colour='#337733'
     )
-    fig.savefig('plot_line_x_y_test_2.svg', format='svg')
-    print(
-        '<p>'
-        '<figure>'
-        '<img src="plot_line_x_y_test_2.svg" alt="alternate text graph 1"/>'
-        '<figcaption>plot_line_x_y_test_2</figcaption>'
-        '</figure>'
-        '</p>'
-    )
+    fig.savefig('plot_line_x_y_intx_test.svg', format='svg')
+    ds.html_figure(filename='plot_line_x_y_intx_test.svg')
     ds.html_end(
         originalstdout=original_stdout,
         outputurl=output_url
