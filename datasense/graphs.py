c3a16ea50ac52b99a27fe69d9a148bd8f2aa724e
Gilles Pilon <gillespilon13@gmail.com>
2020-09-25 13:39:53 -0500

    Add example 3 with 'uniform'

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -407,21 +407,10 @@ def plot_line_x_y(
     >>> plt.show()
 
     Example 3
-    >>> series_x = pd.Series(
-    >>>     rng.uniform(
-    >>>         low=13,
-    >>>         high=69,
-    >>>         size=42
-    >>>     )
-    >>> ).sort_values()
+    >>> series_x = ds.random_data(distribution='uniform').sort_values()
     >>> fig, ax = ds.plot_line_x_y(
     >>>     X=series_x,
-    >>>     y=series_y,
-    >>>     figuresize=(8, 4.5),
-    >>>     marker='o',
-    >>>     markersize=8,
-    >>>     linestyle=':',
-    >>>     colour='#337733'
+    >>>     y=series_y
     >>> )
     >>> plt.show()
     '''
