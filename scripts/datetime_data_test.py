8ab0f0ffedb11e8bc7da9d8cf5a20ed73f8b426c
Gilles Pilon <gillespilon13@gmail.com>
2020-09-25 15:27:09 -0500

    Initial commit

---
+++ scripts/datetime_data_test.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+"""
+Test for datetime data
+"""
+
+import datasense as ds
+
+output_url = 'datetime_data_test.html'
+header_title = 'datetime_data_test'
+header_id = 'datetime-data-test'
+
+
+def main():
+    original_stdout = ds.html_begin(
+        outputurl=output_url,
+        headertitle=header_title,
+        headerid=header_id
+    )
+    print('<pre>')
+    series = ds.datetime_data()
+    print('datetime series')
+    print(series)
+    print('</pre>')
+    ds.html_end(
+        originalstdout=original_stdout,
+        outputurl=output_url
+    )
+
+
+if __name__ == '__main__':
+    main()
