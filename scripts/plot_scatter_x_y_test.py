49fe9054879579d07d1bb7ced37fbd8bae3c8532
Gilles Pilon <gillespilon13@gmail.com>
2020-09-25 14:34:01 -0500

    Use random_data(), refactor, add examples

--- scripts/plot_scatter_x_y_test.py
+++ scripts/plot_scatter_x_y_test.py
@@ -6,11 +6,7 @@ time -f '%e' ./plot_scatter_x_y_test.py
 ./plot_scatter_x_y_test.py
 '''
 
-from numpy.random import default_rng
-from datetime import timedelta
-from numpy import arange
 import datasense as ds
-import pandas as pd
 
 output_url = 'plot_scatter_x_y_test.html'
 header_title = 'plot_scatter_x_y_test'
@@ -23,29 +19,20 @@ def main():
         headertitle=header_title,
         headerid=header_id
     )
-    rng = default_rng()
-    series_x = pd.Series(
-        arange(
-            '2020-01-01T13:13:13',
-            '2020-02-12T13:13:13',
-            timedelta(hours=24),
-            dtype='datetime64[s]'
-        )
-    )
-    series_y = pd.Series(rng.standard_normal(size=42))
+    # Example 1
+    series_x = ds.datetime_data()
+    series_y = ds.random_data()
     fig, ax = ds.plot_scatter_x_y(
         X=series_x,
         y=series_y
     )
-    fig.savefig('plot_scatter_x_y_datex_test.svg', format='svg')
-    ds.html_figure(filename='plot_scatter_x_y_datex_test.svg')
-    series_x = pd.Series(
-        rng.uniform(
-            low=13,
-            high=69,
-            size=42
-        )
+    fig.savefig(
+        'plot_scatter_x_y_datex_test.svg',
+        format='svg'
     )
+    ds.html_figure(filename='plot_scatter_x_y_datex_test.svg')
+    # Example 2
+    series_x = ds.random_data(distribution='randint').sort_values()
     fig, ax = ds.plot_scatter_x_y(
         X=series_x,
         y=series_y,
@@ -54,8 +41,33 @@ def main():
         markersize=8,
         colour='#cc3311'
     )
-    fig.savefig('plot_scatter_x_y_intx_test.svg', format='svg')
+    fig.savefig(
+        'plot_scatter_x_y_intx_test.svg',
+        format='svg'
+    )
     ds.html_figure(filename='plot_scatter_x_y_intx_test.svg')
+    # Example 3
+    series_x = ds.random_data(distribution='uniform').sort_values()
+    fig, ax = ds.plot_scatter_x_y(
+        X=series_x,
+        y=series_y
+    )
+    fig.savefig(
+        fname='plot_scatter_x_y_uniformx_test.svg',
+        format='svg'
+    )
+    ds.html_figure(filename='plot_scatter_x_y_uniformx_test.svg')
+    # Example 4
+    series_x = ds.random_data().sort_values()
+    fig, ax = ds.plot_scatter_x_y(
+        X=series_x,
+        y=series_y
+    )
+    fig.savefig(
+        fname='plot_scatter_x_y_normx_test.svg',
+        format='svg'
+    )
+    ds.html_figure(filename='plot_scatter_x_y_normx_test.svg')
     ds.html_end(
         originalstdout=original_stdout,
         outputurl=output_url
