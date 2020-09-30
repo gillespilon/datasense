a12a2ec32e0ee1990a315f0eaf036f7835862a9f
Gilles Pilon <gillespilon13@gmail.com>
2020-09-30 10:57:29 -0500

    Add html output

--- scripts/exponentially_weighted_average_example.py
+++ scripts/exponentially_weighted_average_example.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 '''
-Exponentially weighted average for an X and Y plot
+Exponentially weighted average plot
 
 This script has several functions:
 
@@ -17,44 +17,27 @@ time -f '%e' ./exponentially_weighted_average.py
 '''
 
 
-from typing import Tuple
+from typing import List, Tuple
+
 import matplotlib.axes as axes
 import matplotlib.cm as cm
-from matplotlib.dates import DateFormatter, DayLocator
-from matplotlib.ticker import NullFormatter, NullLocator
-import matplotlib.pyplot as plt
-import pandas as pd
 import datasense as ds
-
-
-c = cm.Paired.colors
-alpha_value = 1.0
-function = 'mean'
-parser = '%Y-%m-%d %H:%M:%S'
-file_name = [
-    'raw_data_integer_float.csv',
-    'raw_data_datetime_float.csv'
-]
-abscissa_name = ['abscissa', 'datetime']
-ordinate_name = ['ordinate', 'observed']
-ordinate_predicted_name = [
-    'ordinate_predicted',
-    'ordinate_predicted'
-]
-graph_file_name = [
-    'exponentially_weighted_average_integer_float',
-    'exponentially_weighted_average_datetime_float'
-]
-date_time_parser = [None, parser]
-date_formatter = [None, '%m-%d']
-column_names_sort = [False, False]
-figure_width_height = (8, 6)
-x_axis_label = 'Abscissa'
-y_axis_label = 'Ordinate'
-axis_title = 'Exponentially Weighted Average'
+import webbrowser
+import time
+import sys
 
 
 def main():
+    start_time = time.time()
+    global figure_width_height, c, date_time_parser
+    file_names, graph_file_names, abscissa_names, ordinate_names,\
+        ordinate_predicted_names, x_axis_label, y_axis_label, axis_title,\
+        figure_width_height, column_names_sort, date_time_parser,\
+        date_formatter, c, alpha_value, function, output_url,\
+        header_title, header_id, parser = parameters()
+    original_stdout = sys.stdout
+    sys.stdout = open(output_url, 'w')
+    ds.html_header(header_title, header_id)
     for (
         filename,
         abscissaname,
@@ -65,86 +48,132 @@ def main():
         dateformatter,
         graphfilename
     ) in zip(
-        file_name,
-        abscissa_name,
-        ordinate_name,
-        ordinate_predicted_name,
+        file_names,
+        abscissa_names,
+        ordinate_names,
+        ordinate_predicted_names,
         date_time_parser,
         column_names_sort,
         date_formatter,
-        graph_file_name
+        graph_file_names
     ):
-        data = ds.read_file(
-            filename,
-            abscissaname,
-            datetimeparser,
-            columnnamessort
-        )
+        if datetimeparser == 'None':
+            data = ds.read_file(
+                filename=filename,
+                abscissa=abscissaname,
+                columnnamessort=columnnamessort
+            )
+        else:
+            data = ds.read_file(
+                filename=filename,
+                abscissa=abscissaname,
+                datetimeparser=parser,
+                columnnamessort=columnnamessort
+            )
         data[ordinatepredictedname] = data[ordinatename]\
             .ewm(alpha=alpha_value).mean()
-        plot_graph(
-            data,
-            abscissaname,
-            ordinatename,
-            ordinatepredictedname,
-            figure_width_height,
-            dateformatter,
-            graphfilename,
-            axis_title,
-            x_axis_label,
-            y_axis_label
+        fig, ax = ds.plot_scatter_line_x_y1_y2(
+            X=data[abscissaname],
+            y1=data[ordinatename],
+            y2=data[ordinatepredictedname],
+            figuresize=figure_width_height
         )
+        ax.set_title(axis_title, fontweight='bold')
+        ax.set_xlabel(x_axis_label, fontweight='bold')
+        ax.set_ylabel(y_axis_label, fontweight='bold')
+        despine(ax)
+        ax.figure.savefig(f'{graphfilename}.svg', format='svg')
+        print(f'<p><img src="{graphfilename}.svg"/></p>')
+    ds.html_footer()
+    sys.stdout.close()
+    sys.stdout = original_stdout
+    webbrowser.open_new_tab(output_url)
+
+
+def parameters() -> (
+    List[str],
+    List[str],
+    List[str],
+    List[str],
+    List[str],
+    str,
+    str,
+    str,
+    Tuple[float],
+    List[bool],
+    List[str],
+    List[str],
+    str,
+    float,
+    str,
+    str,
+    str,
+    str,
+    str
+):
+    '''
+    Set parameters.
+    '''
+
+    parameters = ds.read_file(
+        filename='exponentially_weighted_average_parameters.ods'
+    )
+    filenames = [x for x in parameters['File names'] if str(x) != 'nan']
+    graphfilenames = [x for x in parameters['Graph file names']
+                      if str(x) != 'nan']
+    abscissanames = [x for x in parameters['Abscissa names']
+                     if str(x) != 'nan']
+    ordinatenames = [x for x in parameters['Ordinate names']
+                     if str(x) != 'nan']
+    ordinatepredictednames = [x for x in parameters['Ordinate predicted names']
+                              if str(x) != 'nan']
+    xaxislabel = parameters['Other parameter values'][0]
+    yaxislabel = parameters['Other parameter values'][1]
+    axistitle = parameters['Other parameter values'][2]
+    figurewidthheight = eval(parameters['Other parameter values'][3])
+    columnnamessort = [x for x in parameters['Column names sort']
+                       if str(x) != 'nan']
+    datetimeparser = [x for x in parameters['Date time parser']
+                      if str(x) != 'nan']
+    parser = parameters['Other parameter values'][4]
+    dateformatter = [None
+                     if split.strip() == 'None' else
+                     split.strip()
+                     for unsplit
+                     in parameters['Date formatter']
+                     if str(unsplit) != 'nan'
+                     for split
+                     in unsplit.split(',')]
+    c = cm.Paired.colors
+    alphavalue = parameters['Other parameter values'][6]
+    function = parameters['Other parameter values'][7]
+    outputurl = parameters['Other parameter values'][8]
+    headertitle = parameters['Other parameter values'][9]
+    headerid = parameters['Other parameter values'][10]
+    return (
+        filenames, graphfilenames, abscissanames, ordinatenames,
+        ordinatepredictednames, xaxislabel, yaxislabel, axistitle,
+        figurewidthheight, columnnamessort, datetimeparser, dateformatter, c,
+        alphavalue, function, outputurl, headertitle, headerid, parser
+    )
 
 
 def despine(ax: axes.Axes) -> None:
-    '''
+    """
     Remove the top and right spines of a graph.
 
-    There is only one x axis, on the bottom, and one y axis, on the left.
-    '''
+    Parameters
+    ----------
+    ax : axes.Axes
+
+    Example
+    -------
+    >>> despine(ax)
+    """
     for spine in 'right', 'top':
         ax.spines[spine].set_visible(False)
 
 
-def plot_graph(
-    df: pd.DataFrame,
-    columnx: str,
-    columny: str,
-    columnz: str,
-    figurewidthheight: Tuple[int, int],
-    dateformat: str,
-    graphname: str,
-    graphtitle: str,
-    xaxislabel: str,
-    yaxislabel: str
-) -> None:
-    fig = plt.figure(figsize=figurewidthheight)
-    ax = fig.add_subplot(111)
-    ax.plot(
-        df[columnx],
-        df[columny],
-        marker='.',
-        linestyle='',
-        color=c[1]
-    )
-    ax.plot(
-        df[columnx],
-        df[columnz],
-        marker=None,
-        linestyle='-',
-        color=c[5]
-    )
-    if dateformat:
-        ax.xaxis.set_major_locator(DayLocator())
-        ax.xaxis.set_minor_locator(NullLocator())
-        ax.xaxis.set_major_formatter(DateFormatter(dateformat))
-        ax.xaxis.set_minor_formatter(NullFormatter())
-    ax.set_title(graphtitle, fontweight='bold')
-    ax.set_xlabel(xaxislabel, fontweight='bold')
-    ax.set_ylabel(yaxislabel, fontweight='bold')
-    despine(ax)
-    ax.figure.savefig(f'{graphname}.svg', format='svg')
-
-
 if __name__ == '__main__':
     main()
+
