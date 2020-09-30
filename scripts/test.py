5f6297ec21415eaf26c5a6df4f4fb906beca2ab0
Gilles Pilon <gillespilon13@gmail.com>
2020-09-30 10:46:26 -0500

    Reaplce rng with ds.random_data

--- scripts/test.py
+++ scripts/test.py
@@ -1,17 +1,16 @@
 #! /usr/bin/env python3
 import matplotlib.pyplot as plt
-from numpy.random import default_rng
 import datasense as ds
-import pandas as pd
-
-rng = default_rng()
-data = rng.standard_normal(size=42)
-series = pd.Series(data)
-fig, ax = ds.plot_scatter_y(y=series)
-plt.show()
-
-rng = default_rng()
-data = rng.standard_normal(size=42)
-series = pd.Series(data)
-fig, ax = ds.plot_scatter_y(y=series)
+X = ds.random_data(distribution='uniform').sort_values()
+y = ds.random_data(distribution='norm')
+p = ds.natural_cubic_spline(
+    X=X,
+    y=y,
+    numberknots=10
+)
+fig, ax = ds.plot_scatter_line_x_y1_y2(
+    X=X,
+    y1=y,
+    y2=p.predict(X)
+)
 plt.show()
