bb6f331ae5c3d874ce3ec0fb077858413babd6db
Gilles Pilon <gillespilon13@gmail.com>
2020-10-01 15:16:16 -0500

    Initial commit

---
+++ scripts/pandas_read_csv_exploration.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python3
+"""
+Pandas read_csv exploration
+"""
+
+from typing import Optional, Union
+
+import datasense as ds
+import pandas as pd
+
+
+def main():
+    pd.options.display.max_columns = 500
+    df = create_dataframe()
+    print(df.head())
+    save_dataframe(df=df)
+    data = read_file(
+        file_name='myfile.csv'
+    )
+    print(data.head())
+
+
+def create_dataframe() -> pd.DataFrame:
+    df = pd.DataFrame(
+        {
+            'b': ds.random_data(distribution='bool'),
+            'c': ds.random_data(distribution='categories'),
+            'd': ds.timedelta_data(),
+            's': ds.random_data(distribution='strings'),
+            't': ds.datetime_data(),
+            'x': ds.random_data(distribution='norm'),
+            'y': ds.random_data(distribution='randint'),
+            'z': ds.random_data(distribution='uniform')
+        }
+    )
+    return df
+
+
+def save_dataframe(df) -> None:
+    df.to_csv(
+        'myfile.csv',
+        index=False
+    )
+
+
+def read_file(
+    file_name: str,
+    *,
+    index_col: Optional[Union[str, bool]] = None
+) -> pd.DataFrame:
+    df = pd.read_csv(
+        file_name
+    )
+    """
+    Create a DataFrame from an external file.
+
+    Parameters
+    ----------
+    file_name : str
+        The name of the file to read.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        The dataframe created from the external file.
+
+    Examples
+    --------
+    Example 1
+    >>> data = read_file(file_name='myfile.csv')
+    """
+    return df
+
+
+if __name__ == '__main__':
+    main()
