699153fa080f0fe06c7e94df59723e29a6de4646
Gilles Pilon <gillespilon13@gmail.com>
2020-10-06 14:04:37 -0500

    Add example 4

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -767,6 +767,27 @@ def plot_scatter_scatter_x1_x2_y1_y2(
     >>> )
     >>> ax.legend(frameon=False)
     >>> plt.show()
+
+    Example 4
+    >>> fig, ax = ds.plot_scatter_scatter_x1_x2_y1_y2(
+    >>>     X1=series_x1,
+    >>>     X2=series_x2,
+    >>>     y1=series_y1,
+    >>>     y2=series_y2,
+    >>>     figuresize=(8, 5),
+    >>>     marker1='o',
+    >>>     marker2='+',
+    >>>     markersize1=8,
+    >>>     markersize2=12,
+    >>>     colour1='#cc3311',
+    >>>     colour2='#ee3377',
+    >>>     labellegendy1='y1',
+    >>>     labellegendy2='y2',
+    >>>     smoothing='natural_cubic_spline',
+    >>>     numknots=7
+    >>> )
+    >>> ax.legend(frameon=False)
+    >>> plt.show()
     '''
     fig = plt.figure(figsize=figuresize)
     ax = fig.add_subplot(111)
