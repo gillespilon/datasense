466b3a490ec1ebc5dfb2f36cf312a398e6a61259
Gilles Pilon <gillespilon13@gmail.com>
2020-10-06 11:18:37 -0500

    Ad def plot_scatter_scatter_x1_x2_y1_y2()

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -637,6 +637,197 @@ def plot_scatter_scatter_x_y1_y2(
     return (fig, ax)
 
 
+def plot_scatter_scatter_x1_x2_y1_y2(
+    X1: pd.Series,
+    X2: pd.Series,
+    y1: pd.Series,
+    y2: pd.Series,
+    *,
+    figuresize: Optional[Tuple[float, float]] = None,
+    smoothing: Optional[str] = None,
+    numknots: Optional[int] = None,
+    marker1: Optional[str] = '.',
+    marker2: Optional[str] = '.',
+    markersize1: Optional[int] = 8,
+    markersize2: Optional[int] = 8,
+    linestyle1: Optional[str] = 'None',
+    linestyle2: Optional[str] = 'None',
+    linewidth1: Optional[float] = 1,
+    linewidth2: Optional[float] = 1,
+    colour1: Optional[str] = '#0077bb',
+    colour2: Optional[str] = '#33bbee',
+    labellegendy1: Optional[str] = None,
+    labellegendy2: Optional[str] = None
+) -> Tuple[plt.figure, axes.Axes]:
+    '''
+    Scatter plot of y1 versus X1.
+    Scatter plot of y2 versus X2.
+    Optional smoothing applied to y1, y2.
+
+    This graph is useful if y1 and y2 have the same units.
+
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
+    Fit a piecewise cubic function the the constraint that the fitted curve is
+    linear outside the range of the knots. The fitter curve is continuously
+    differentiable to the second order at all of the knots.
+
+    Parameters
+    ----------
+    X1 : pd.Series
+        The data to plot on the abscissa.
+    X2 : pd.Series
+        The data to plot on the abscissa.
+    y1 : pd.Series
+        The data to plot on the ordinate.
+    y2 : pd.Series
+        The data to plot on the ordinate.
+    figuresize : Optional[Tuple[float, float]] = None
+        The (width, height) of the figure (in, in).
+    smoothing : Optional[str] = None
+        The type of smoothing to apply.
+    numknots : Optional[int] = None
+        The number of knots for natural cubic spline smoothing.
+    marker1 : Optional[str] = '.'
+        The type of plot point for y1.
+    marker2 : Optional[str] = '.'
+        The type of plot point for y2.
+    markersize1 : Optional[int] = 8
+        The size of the plot point for y1.
+    markersize2 : Optional[int] = 8
+        The size of the plot point for y2.
+    linestyle1 : Optional[str] = 'None'
+        The style of the line for y1.
+    linestyle2 : Optional[str] = 'None'
+        The style of the line for y2.
+    linewidth1 : Optional[float] = 0
+        The width of the line for y1.
+    linewidth2 : Optional[float] = 0
+        The width of the line for y2.
+    colour1 : Optional[str] = '#0077bb'
+        The colour of the line for y1.
+    colour2 : Optional[str] = '#33bbee'
+        The colour of the line for y2.
+    labellegendy1 : Optional[str] = None
+        The legend label of the line y1.
+    labellegendy2 : Optional[str] = None
+        The legend label of the line y2.
+
+    Returns
+    -------
+    Tuple[plt.figure, axes.Axes]
+        A matplotlib figure and Axes tuple.
+
+    Examples
+    --------
+    Example 1
+    >>> from numpy.random import default_rng
+    >>> import matplotlib.pyplot as plt
+    >>> import datasense as ds
+    >>> import pandas as pd
+    >>>
+    >>> rng = default_rng()
+    >>> series_x = Series(
+    >>>     arange(
+    >>>         '2020-01-01T13:13:13',
+    >>>         '2020-02-12T13:13:13',
+    >>>         timedelta(hours=24),
+    >>>         dtype='datetime64[s]'
+    >>>     )
+    >>> )
+    >>> series_y1 = ds.random_data()
+    >>> series_y2 = ds.random_data()
+    >>> fig, ax = ds.plot_scatter_scatter_x_y1_y2(
+    >>>     X=series_x,
+    >>>     y1=series_y1,
+    >>>     y2=series_y2
+    >>> )
+    >>> plt.show()
+
+    Example 2
+    >>> series_x = ds.random_data(distribution='uniform')
+    >>> fig, ax = ds.plot_scatter_scatter_x_y1_y2(
+    >>>     X=series_x,
+    >>>     y1=series_y1,
+    >>>     y2=series_y2,
+    >>>     figuresize=(8, 5),
+    >>>     marker1='o',
+    >>>     marker2='+',
+    >>>     markersize1=8,
+    >>>     markersize2=12,
+    >>>     colour1='#cc3311',
+    >>>     colour2='#ee3377',
+    >>>     labellegendy1='y1',
+    >>>     labellegendy2='y2'
+    >>> )
+    >>> ax.legend(frameon=False)
+    >>> plt.show()
+    '''
+    fig = plt.figure(figsize=figuresize)
+    ax = fig.add_subplot(111)
+    if smoothing is None:
+        if (X1.dtype and X2.dtype) in ['datetime64[ns]']:
+            format_dates(fig, ax)
+        ax.plot(
+            X1,
+            y1,
+            marker=marker1,
+            markersize=markersize1,
+            linestyle=linestyle1,
+            linewidth=linewidth1,
+            color=colour1,
+            label=labellegendy1
+        )
+        ax.plot(
+            X2,
+            y2,
+            marker=marker2,
+            markersize=markersize2,
+            linestyle=linestyle2,
+            linewidth=linewidth2,
+            color=colour2,
+            label=labellegendy2
+        )
+    elif smoothing == 'natural_cubic_spline':
+        if (X1.dtype and X2.dtype) in ['datetime64[ns]']:
+            XX1 = pd.to_numeric(X1)
+            XX2 = pd.to_numeric(X2)
+            fig.autofmt_xdate()
+        else:
+            XX1 = X1
+            XX2 = X2
+        model1 = natural_cubic_spline(
+            X1=XX1,
+            y=y1,
+            numberknots=numknots
+        )
+        model2 = natural_cubic_spline(
+            X2=XX2,
+            y=y2,
+            numberknots=numknots
+        )
+        ax.plot(
+            X1,
+            model1.predict(X1),
+            marker=marker1,
+            markersize=markersize1,
+            linestyle='None',
+            linewidth=linewidth1,
+            color=colour1
+        )
+        ax.plot(
+            X2,
+            model2.predict(X2),
+            marker=marker2,
+            markersize=markersize2,
+            linestyle='None',
+            linewidth=linewidth2,
+            color=colour2
+        )
+        ax.plot(X1, model1.predict(XX1), marker='.', linestyle='', color=c[1])
+        ax.plot(X2, model2.predict(XX2), marker='.', linestyle='', color=c[5])
+    return (fig, ax)
+
+
 def plot_scatter_line_x_y1_y2(
     X: pd.Series,
     y1: pd.Series,
@@ -1059,6 +1250,7 @@ __all__ = (
     'plot_line_y',
     'plot_line_x_y',
     'plot_scatter_scatter_x_y1_y2',
+    'plot_scatter_scatter_x1_x2_y1_y2',
     'plot_scatter_line_x_y1_y2',
     'plot_line_line_x_y1_y2',
     'plot_line_line_line_x_y1_y2_y3',
