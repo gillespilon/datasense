7ea5b902d18006d3390319366a24d9edb8bd1939
Gilles Pilon <gillespilon13@gmail.com>
2020-10-07 19:24:20 -0500

    Add pd.set_option examples

--- scripts/pandas_read_csv_exploration.py
+++ scripts/pandas_read_csv_exploration.py
@@ -16,7 +16,18 @@ header_id = 'pandas-read-csv-exploration'
 
 
 def main():
-    # pd.options.display.max_columns = 500
+    pd.set_option(
+        'display.width',
+        120
+    )
+    pd.set_option(
+        'display.max_columns',
+        100
+    )
+    pd.set_option(
+        'display.max_rows',
+        100
+    )
     original_stdout = ds.html_begin(
         outputurl=output_url,
         headertitle=header_title,
