16c6f02703be17c0468af72fdacf0c75b86194f4
Gilles Pilon <gillespilon13@gmail.com>
2020-10-19 10:47:06 -0500

    Fix "_" in parameters

--- scripts/plot_scatter_x_y_test.py
+++ scripts/plot_scatter_x_y_test.py
@@ -15,9 +15,9 @@ header_id = 'plot-scatter-x-y-test'
 
 def main():
     original_stdout = ds.html_begin(
-        outputurl=output_url,
-        headertitle=header_title,
-        headerid=header_id
+        output_url=output_url,
+        header_title=header_title,
+        header_id=header_id
     )
     # Example 1
     series_x = ds.datetime_data()
@@ -69,8 +69,8 @@ def main():
     )
     ds.html_figure(file_name='plot_scatter_x_y_normx_test.svg')
     ds.html_end(
-        originalstdout=original_stdout,
-        outputurl=output_url
+        original_stdout=original_stdout,
+        output_url=output_url
     )
 
 
