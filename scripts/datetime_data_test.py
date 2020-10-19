0affc76a65ab445bd50d5f3d39342536b5f88464
Gilles Pilon <gillespilon13@gmail.com>
2020-10-19 10:48:15 -0500

    Fix "_" in parameters

--- scripts/datetime_data_test.py
+++ scripts/datetime_data_test.py
@@ -12,9 +12,9 @@ header_id = 'datetime-data-test'
 
 def main():
     original_stdout = ds.html_begin(
-        outputurl=output_url,
-        headertitle=header_title,
-        headerid=header_id
+        output_url=output_url,
+        header_title=header_title,
+        header_id=header_id
     )
     print('<pre style="white-space: pre-wrap;">')
     series = ds.datetime_data()
@@ -22,8 +22,8 @@ def main():
     print(series)
     print('</pre>')
     ds.html_end(
-        originalstdout=original_stdout,
-        outputurl=output_url
+        original_stdout=original_stdout,
+        output_url=output_url
     )
 
 
