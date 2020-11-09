01aa6b9d8b29e1942dcb88b57b3c11eb3a5dd1df
Gilles Pilon <gillespilon13@gmail.com>
2020-11-09 13:51:15 -0600

    Use pd.options.display.max_columns

--- scripts/pandas_read_file_exploration.py
+++ scripts/pandas_read_file_exploration.py
@@ -21,10 +21,7 @@ file_name='myfile.csv'
 def main():
     start_time = time.time()
     pd.options.display.width = 120
-    pd.set_option(
-        'display.max_columns',
-        100
-    )
+    pd.options.display.max_columns = 100
     pd.set_option(
         'display.max_rows',
         100
