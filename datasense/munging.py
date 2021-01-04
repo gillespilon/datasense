8550475726aca6ba4336fdc075d3c9fdf939b4f6
Gilles Pilon <gillespilon13@gmail.com>
2021-01-04 14:25:10 -0600

    Tidy files in list_files_all()

--- datasense/munging.py
+++ datasense/munging.py
@@ -1586,9 +1586,7 @@ def list_files_all(directory: str) -> List[str]:
     -------
     >>> from pathlib import Path
     >>> directory = (Path.cwd() / 'directoryname')
-    >>> files = list_files_with_patterns(
-    >>>     directory=directory
-    >>> )
+    >>> files = list_files_with_patterns(directory=directory)
     """
     files = [file.name for file in directory.glob('*.*')]
     return files
