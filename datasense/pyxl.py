f86d26a5409a02941c9371c89b04c6aff6e76963
Gilles Pilon <gillespilon13@gmail.com>
2021-02-19 09:09:54 -0600

    Re-order functions alphabetically

--- datasense/pyxl.py
+++ datasense/pyxl.py
@@ -17,45 +17,148 @@ import numpy as np
 import openpyxl
 
 
-def style_header(
-    *,
-    font_bold: Optional[bool] = True,
-    align_horizontal: Optional[str] = 'center',
-    align_vertical: Optional[str] = 'center',
-    fill_type: Optional[str] = 'solid',
-    color_foreground: Optional[str] = 'd9d9d9'
-):
+def change_case_worksheet_columns(
+    ws: openpyxl.worksheet.worksheet.Worksheet,
+    min_col: int,
+    max_col: int,
+    min_row: int,
+    max_row: int,
+    case: str = 'upper'
+) -> openpyxl.worksheet.worksheet.Worksheet:
     """
-    Define a style typically used to style the first row of a worksheet.
+    Change case for one or more worksheet columns.
 
     Parameters
     ----------
-    font_bold : Optional[bool] = True
-        The boolean for making a font bold.
-    align_horizontal : Optional[str] = 'center'
-        The string for horizontal alignment of a cell.
-    align_vertical : Optional[str] = 'center'
-        The string for vertical alignment of a cell.
-    fill_type : Optional[str] = 'solid'
-        The string for the type of fill of a cell.
-    color_foreground : Optional[str] = 'd9d9d9'
-        The string for the foreground color of a cell.
+    ws : openpyxl.worksheet.worksheet.Worksheet
+        The worksheet in which to change the case of column(s).
+    min_col : int
+        The first column in the range to change.
+    max_col : int
+        The last column in the range to change.
+    min_row : int
+        The first row in the range to change.
+    max_row : int
+        The last row in the range to change.
+    case: str = 'upper'
+        The case to change. Currently only upper or lower.
+
+    Returns
+    -------
+    ws : openpyxl.worksheet.worksheet.Worksheet
+        A worksheet from a workbook.
 
     Example
     -------
     >>> import datasense as ds
-    >>> ds.style_header()
+    >>> ws = ds.change_case_worksheet_columns(
+    >>>     ws=ws,
+    >>>     min_col=4,
+    >>>     max_col=6,
+    >>>     min_row=1,
+    >>>     max_row=ws.max_row,
+    >>>     case='upper'
+    >>> )
     """
-    header_style = NamedStyle(name='header_style')
-    header_style.font = Font(bold=font_bold)
-    header_style.alignment = Alignment(
-        horizontal=align_horizontal,
-        vertical=align_vertical
+    for col in ws.iter_cols(
+        min_col=min_col,
+        max_col=max_col,
+        min_row=min_row,
+        max_row=max_row
+    ):
+        for cell in col:
+            if case == 'upper':
+                cell.value = str(cell.value).upper()
+            elif case == 'lower':
+                cell.value = str(cell.value).lower()
+    return ws
+
+
+def exit_script(
+    original_stdout,
+    output_url
+):
+    html_end(
+        original_stdout=original_stdout,
+        output_url=output_url
     )
-    header_style.fill = PatternFill(
-        fill_type=fill_type,
-        fgColor=color_foreground
-    )
+    sys.exit()
+
+
+def list_duplicate_worksheet_rows(
+    ws: openpyxl.worksheet.worksheet.Worksheet
+) -> List[int]:
+    """
+    Find duplicate rows in a worksheet.
+
+    Parameters
+    ----------
+    ws : openpyxl.worksheet.worksheet.Worksheet
+        A worksheet from a workbook.
+
+    Returns
+    -------
+    duplicate_rows : List[int]
+        A list of duplicate row numbers.
+
+    Example
+    -------
+    >>> import datasense as ds
+    >>> duplicate_rows = ds.list_duplicate_worksheet_rows(ws=ws)
+    >>> ws = ds.remove_worksheet_rows(
+    >>>     ws=ws,
+    >>>     duplicate_rows=duplicate_rows
+    >>> )
+    """
+    duplicate_rows = []
+    unique_rows = []
+    for row in ws.rows:
+        working_list = []
+        for cell in row:
+            working_list.append(cell.value)
+        if working_list not in unique_rows:
+            unique_rows.append(working_list)
+        else:
+            duplicate_rows.append(cell.row)
+    return duplicate_rows
+
+
+def list_empty_and_nan_worksheet_rows(
+    ws: openpyxl.worksheet.worksheet.Worksheet,
+    min_row: int
+) -> List[int]:
+    """
+    Create list of row numbers of blank worksheet rows.
+
+    Parameters
+    ----------
+    ws : openpyxl.worksheet.worksheet.Worksheet
+        A worksheet from a workbook.
+    min_row : int
+        Start row for iteration.
+
+    Returns
+    -------
+    blank_rows : List[int]
+        List of row numbers.
+
+    Example
+    -------
+    >>> import datasense as ds
+    >>> ws = wb[sheetname]
+    >>> blank_rows = ds.list_nan_worksheet_rows(
+    >>>     ws=ws,
+    >>>     min_row=2
+    >>> )
+    """
+    blank_rows = []
+    for row in ws.iter_rows(min_row=min_row):
+        onerow = [cell.value for cell in row]
+        if all(item in [
+            None, 'None', 'NONE', '', np.nan
+        ] for item in onerow):
+            blank_rows.append(row[0].row)
+    return blank_rows
 
 
 def list_empty_except_nan_worksheet_rows(
@@ -95,6 +198,85 @@ def list_empty_except_nan_worksheet_rows(
         if all(item == onerow[0] for item in onerow):
             empty_rows.append(row[0].row)
     return empty_rows
+
+
+def list_nan_worksheet_rows(
+    ws: openpyxl.worksheet.worksheet.Worksheet,
+    min_row: int
+) -> List[int]:
+    """
+    Create list of row numbers of blank worksheet rows.
+
+    Parameters
+    ----------
+    ws : openpyxl.worksheet.worksheet.Worksheet
+        A worksheet from a workbook.
+    min_row : int
+        Start row for iteration.
+
+    Returns
+    -------
+    blank_rows : List[int]
+        List of row numbers.
+
+    Example
+    -------
+    >>> import datasense as ds
+    >>> ws = wb[sheetname]
+    >>> blank_rows = ds.list_nan_worksheet_rows(
+    >>>     ws=ws,
+    >>>     min_row=2
+    >>> )
+    """
+    blank_rows = []
+    for row in ws.iter_rows(min_row=min_row):
+        onerow = [cell.value for cell in row]
+        if all(item != item for item in onerow):
+            blank_rows.append(row[0].row)
+    return blank_rows
+
+
+def read_workbook(
+    filename: Union[Path, str],
+    *,
+    data_only: Optional[bool] = True
+) -> Tuple[openpyxl.workbook.Workbook, List[str]]:
+    """
+    Read a workbook, print the Path, and print the sheet names.
+
+    Parameters
+    ----------
+    filename : Union[Path, str]
+        The file containing the workbook.
+    data_only : Optional[bool] = True
+        If True, read values stored in the cells. If False, read formulae
+        stored in the cells.
+
+    Returns
+    -------
+    wb : openpyxl.workbook.Workbook
+        A workbook.
+    sheet_names : List[str]
+        The sheet names in the workbook.
+
+    Examples
+    --------
+    >>> import datasense as ds
+    >>> wb, sheet_names = ds.read_workbook(
+    >>>     filename=file,
+    >>>     data_only=True
+    >>> )
+    """
+    wb = load_workbook(
+        filename=filename,
+        data_only=data_only
+    )
+    print('File:')
+    print(filename)
+    print('Sheet names found:')
+    sheet_names = wb.sheetnames
+    print(wb.sheetnames)
+    return (wb, sheet_names)
 
 
 def remove_empty_worksheet_rows(
@@ -168,259 +350,45 @@ def remove_worksheet_rows(
     return ws
 
 
-def list_duplicate_worksheet_rows(
-    ws: openpyxl.worksheet.worksheet.Worksheet
-) -> List[int]:
-    """
-    Find duplicate rows in a worksheet.
-
-    Parameters
-    ----------
-    ws : openpyxl.worksheet.worksheet.Worksheet
-        A worksheet from a workbook.
-
-    Returns
-    -------
-    duplicate_rows : List[int]
-        A list of duplicate row numbers.
-
-    Example
-    -------
-    >>> import datasense as ds
-    >>> duplicate_rows = ds.list_duplicate_worksheet_rows(ws=ws)
-    >>> ws = ds.remove_worksheet_rows(
-    >>>     ws=ws,
-    >>>     duplicate_rows=duplicate_rows
-    >>> )
-    """
-    duplicate_rows = []
-    unique_rows = []
-    for row in ws.rows:
-        working_list = []
-        for cell in row:
-            working_list.append(cell.value)
-        if working_list not in unique_rows:
-            unique_rows.append(working_list)
-        else:
-            duplicate_rows.append(cell.row)
-    return duplicate_rows
-
-
-def read_workbook(
-    filename: Union[Path, str],
+def style_header(
     *,
-    data_only: Optional[bool] = True
-) -> Tuple[openpyxl.workbook.Workbook, List[str]]:
+    font_bold: Optional[bool] = True,
+    align_horizontal: Optional[str] = 'center',
+    align_vertical: Optional[str] = 'center',
+    fill_type: Optional[str] = 'solid',
+    color_foreground: Optional[str] = 'd9d9d9'
+):
     """
-    Read a workbook, print the Path, and print the sheet names.
+    Define a style typically used to style the first row of a worksheet.
 
     Parameters
     ----------
-    filename : Union[Path, str]
-        The file containing the workbook.
-    data_only : Optional[bool] = True
-        If True, read values stored in the cells. If False, read formulae
-        stored in the cells.
+    font_bold : Optional[bool] = True
+        The boolean for making a font bold.
+    align_horizontal : Optional[str] = 'center'
+        The string for horizontal alignment of a cell.
+    align_vertical : Optional[str] = 'center'
+        The string for vertical alignment of a cell.
+    fill_type : Optional[str] = 'solid'
+        The string for the type of fill of a cell.
+    color_foreground : Optional[str] = 'd9d9d9'
+        The string for the foreground color of a cell.
 
-    Returns
+    Example
     -------
-    wb : openpyxl.workbook.Workbook
-        A workbook.
-    sheet_names : List[str]
-        The sheet names in the workbook.
-
-    Examples
-    --------
     >>> import datasense as ds
-    >>> wb, sheet_names = ds.read_workbook(
-    >>>     filename=file,
-    >>>     data_only=True
-    >>> )
+    >>> ds.style_header()
     """
-    wb = load_workbook(
-        filename=filename,
-        data_only=data_only
+    header_style = NamedStyle(name='header_style')
+    header_style.font = Font(bold=font_bold)
+    header_style.alignment = Alignment(
+        horizontal=align_horizontal,
+        vertical=align_vertical
     )
-    print('File:')
-    print(filename)
-    print('Sheet names found:')
-    sheet_names = wb.sheetnames
-    print(wb.sheetnames)
-    return (wb, sheet_names)
-
-
-def change_case_worksheet_columns(
-    ws: openpyxl.worksheet.worksheet.Worksheet,
-    min_col: int,
-    max_col: int,
-    min_row: int,
-    max_row: int,
-    case: str = 'upper'
-) -> openpyxl.worksheet.worksheet.Worksheet:
-    """
-    Change case for one or more worksheet columns.
-
-    Parameters
-    ----------
-    ws : openpyxl.worksheet.worksheet.Worksheet
-        The worksheet in which to change the case of column(s).
-    min_col : int
-        The first column in the range to change.
-    max_col : int
-        The last column in the range to change.
-    min_row : int
-        The first row in the range to change.
-    max_row : int
-        The last row in the range to change.
-    case: str = 'upper'
-        The case to change. Currently only upper or lower.
-
-    Returns
-    -------
-    ws : openpyxl.worksheet.worksheet.Worksheet
-        A worksheet from a workbook.
-
-    Example
-    -------
-    >>> import datasense as ds
-    >>> ws = ds.change_case_worksheet_columns(
-    >>>     ws=ws,
-    >>>     min_col=4,
-    >>>     max_col=6,
-    >>>     min_row=1,
-    >>>     max_row=ws.max_row,
-    >>>     case='upper'
-    >>> )
-    """
-    for col in ws.iter_cols(
-        min_col=min_col,
-        max_col=max_col,
-        min_row=min_row,
-        max_row=max_row
-    ):
-        for cell in col:
-            if case == 'upper':
-                cell.value = str(cell.value).upper()
-            elif case == 'lower':
-                cell.value = str(cell.value).lower()
-    return ws
-
-
-def write_dataframe_to_worksheet(
-    ws: openpyxl.worksheet.worksheet.Worksheet,
-    df: pd.DataFrame,
-    index: bool = False,
-    header: bool = True
-) -> openpyxl.worksheet.worksheet.Worksheet:
-    """
-    Write a dataframe to a worksheet.
-
-    Parameters
-    ----------
-    ws : openpyxl.worksheet.worksheet.Worksheet
-        The worksheet to which the dataframe will be written.
-    df : pd.DataFrame
-        The dataframe of data.
-    index : bool = False
-        Boolean to determine if dataframe index is written to worksheet.
-    header : bool = True
-        Boolean to determine if dataframe header is written to worksheet.
-
-    Returns
-    -------
-    ws : openpyxl.worksheet.worksheet.Worksheet
-        The worksheet created.
-
-    Example
-    >>> import datasense as ds
-    >>> ws = ds.write_dataframe_to_worksheet(
-    >>>     ws=ws,
-    >>>     df=df,
-    >>>     index=False,
-    >>>     header=True
-    >>> )
-    """
-    for row in dataframe_to_rows(
-        df=df,
-        index=index,
-        header=header
-    ):
-        ws.append(row)
-    return ws
-
-
-def list_nan_worksheet_rows(
-    ws: openpyxl.worksheet.worksheet.Worksheet,
-    min_row: int
-) -> List[int]:
-    """
-    Create list of row numbers of blank worksheet rows.
-
-    Parameters
-    ----------
-    ws : openpyxl.worksheet.worksheet.Worksheet
-        A worksheet from a workbook.
-    min_row : int
-        Start row for iteration.
-
-    Returns
-    -------
-    blank_rows : List[int]
-        List of row numbers.
-
-    Example
-    -------
-    >>> import datasense as ds
-    >>> ws = wb[sheetname]
-    >>> blank_rows = ds.list_nan_worksheet_rows(
-    >>>     ws=ws,
-    >>>     min_row=2
-    >>> )
-    """
-    blank_rows = []
-    for row in ws.iter_rows(min_row=min_row):
-        onerow = [cell.value for cell in row]
-        if all(item != item for item in onerow):
-            blank_rows.append(row[0].row)
-    return blank_rows
-
-
-def list_empty_and_nan_worksheet_rows(
-    ws: openpyxl.worksheet.worksheet.Worksheet,
-    min_row: int
-) -> List[int]:
-    """
-    Create list of row numbers of blank worksheet rows.
-
-    Parameters
-    ----------
-    ws : openpyxl.worksheet.worksheet.Worksheet
-        A worksheet from a workbook.
-    min_row : int
-        Start row for iteration.
-
-    Returns
-    -------
-    blank_rows : List[int]
-        List of row numbers.
-
-    Example
-    -------
-    >>> import datasense as ds
-    >>> ws = wb[sheetname]
-    >>> blank_rows = ds.list_nan_worksheet_rows(
-    >>>     ws=ws,
-    >>>     min_row=2
-    >>> )
-    """
-    blank_rows = []
-    for row in ws.iter_rows(min_row=min_row):
-        onerow = [cell.value for cell in row]
-        if all(item in [
-            None, 'None', 'NONE', '', np.nan
-        ] for item in onerow):
-            blank_rows.append(row[0].row)
-    return blank_rows
+    header_style.fill = PatternFill(
+        fill_type=fill_type,
+        fgColor=color_foreground
+    )
 
 
 def validate_sheet_names(
@@ -498,28 +466,60 @@ def validate_sheet_names(
     return wb
 
 
-def exit_script(
-    original_stdout,
-    output_url
-):
-    html_end(
-        original_stdout=original_stdout,
-        output_url=output_url
-    )
-    sys.exit()
+def write_dataframe_to_worksheet(
+    ws: openpyxl.worksheet.worksheet.Worksheet,
+    df: pd.DataFrame,
+    index: bool = False,
+    header: bool = True
+) -> openpyxl.worksheet.worksheet.Worksheet:
+    """
+    Write a dataframe to a worksheet.
+
+    Parameters
+    ----------
+    ws : openpyxl.worksheet.worksheet.Worksheet
+        The worksheet to which the dataframe will be written.
+    df : pd.DataFrame
+        The dataframe of data.
+    index : bool = False
+        Boolean to determine if dataframe index is written to worksheet.
+    header : bool = True
+        Boolean to determine if dataframe header is written to worksheet.
+
+    Returns
+    -------
+    ws : openpyxl.worksheet.worksheet.Worksheet
+        The worksheet created.
+
+    Example
+    >>> import datasense as ds
+    >>> ws = ds.write_dataframe_to_worksheet(
+    >>>     ws=ws,
+    >>>     df=df,
+    >>>     index=False,
+    >>>     header=True
+    >>> )
+    """
+    for row in dataframe_to_rows(
+        df=df,
+        index=index,
+        header=header
+    ):
+        ws.append(row)
+    return ws
 
 
 __all__ = (
-    'style_header',
+    'change_case_worksheet_columns',
+    'exit_script',
+    'list_duplicate_worksheet_rows',
+    'list_empty_and_nan_worksheet_rows',
     'list_empty_except_nan_worksheet_rows',
+    'list_nan_worksheet_rows',
+    'read_workbook',
     'remove_empty_worksheet_rows',
     'remove_worksheet_rows',
-    'list_duplicate_worksheet_rows',
-    'read_workbook',
-    'change_case_worksheet_columns',
-    'write_dataframe_to_worksheet',
-    'list_nan_worksheet_rows',
+    'style_header',
     'validate_sheet_names',
-    'exit_script',
-    'list_empty_and_nan_worksheet_rows',
+    'write_dataframe_to_worksheet',
 )
