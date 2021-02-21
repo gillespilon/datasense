e6a5cb4f24325d6f205a290b01f74bc5ab98e7b0
Gilles Pilon <gillespilon13@gmail.com>
2021-02-20 19:14:13 -0600

    Update type hints for list_nan_worksheet_rows()

--- datasense/pyxl.py
+++ datasense/pyxl.py
@@ -221,7 +221,7 @@ def list_empty_except_nan_worksheet_rows(
 
 
 def list_nan_worksheet_rows(
-    ws: openpyxl.worksheet.worksheet.Worksheet,
+    ws: Worksheet,
     min_row: int
 ) -> List[int]:
     """
@@ -229,7 +229,7 @@ def list_nan_worksheet_rows(
 
     Parameters
     ----------
-    ws : openpyxl.worksheet.worksheet.Worksheet
+    ws : Worksheet
         A worksheet from a workbook.
     min_row : int
         Start row for iteration.
