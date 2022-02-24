cbf22e0b7b72251392cde635e76d64e9ba866020
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-02-24 10:33:26 -0600

    Edit docstring plot_scatter_line_x_y1_y2

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -937,7 +937,14 @@ def plot_scatter_line_x_y1_y2(
 
     This graph is useful if y1 and y2 have the same units.
 
-    x : pd.Series
+    If smoothing is applied, the series must not contain NaN, inf, or -inf.
+    Fit a piecewise cubic function the the constraint that the fitted curve is
+    linear outside the range of the knots. The fitter curve is continuously
+    differentiable to the second order at all of the knots.
+
+    Parameters
+    ----------
+    X : pd.Series
         The series for the horizontal axis.
     y1 : pd.Series
         The series for y1 to plot on the vertical axis.
@@ -945,23 +952,23 @@ def plot_scatter_line_x_y1_y2(
         The series for y2 to plot on the vertical axis.
     figsize : Tuple[float, float] = None
         The (width, height) of the figure (in, in).
-    smoothing: str
+    smoothing: str = None
         Optional: 'natural_cubic_spline'
+    number_knots: positive integer
+        The number of knots to create.
     colour1 : str = '#0077bb'
-        The colour of the y1.
+        The colour of y1.
     colour2 : str = '#33bbee'
         The colour of y2.
     labellegendy1 : str = None
         The legend for y1.
     labellegendy2 : str = None
-        The legend for the y2.
-    number_knots: positive integer
-        The number of knots to create.
+        The legend for y2.
 
-    If smoothing is applied, the series must not contain NaN, inf, or -inf.
-    Fit a piecewise cubic function the the constraint that the fitted curve is
-    linear outside the range of the knots. The fitter curve is continuously
-    differentiable to the second order at all of the knots.
+    Returns
+    -------
+    Tuple[plt.Figure, axes.Axes]
+        A matplotlib figure and Axes tuple.
     '''
     fig, ax = plt.subplots(
         nrows=1,
