93e9ee68a6dd8a2bbaf3a9f698863fd8336f87c8
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-03-20 10:47:21 -0500

    Create plot_box_and_whisker_plot.py

---
+++ scripts/plot_box_and_whisker_plot.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+"""
+Example of box-and-whisker plot.
+"""
+
+from pathlib import Path
+
+import datasense as ds
+
+
+def main():
+    series = ds.random_data()
+    fig, ax = ds.plot_boxplot(series=series)
+    ax.set_title(label='Box-and-whisker plot')
+    ds.despine(ax=ax)
+    ax.set_xticks(ticks=[1], labels=['series'])
+    ax.set_ylabel('y')
+    fig.savefig(fname=Path('boxplot.svg'))
+
+
+if __name__ == '__main__':
+    main()
