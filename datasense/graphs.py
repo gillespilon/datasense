06166514a72f9b03ec6c398ecb3fee2da4b3817e
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-04-09 10:49:34 -0500

    Use variable for colour_cyan

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -2139,7 +2139,7 @@ def plot_histogram(
     >>>     bin_range=(30, 110),
     >>>     figsize=(10,8),
     >>>     bin_label_bool=True,
-    >>>     color="#33bbee"
+    >>>     color=colour_cyan
     >>> )
     """
     fig, ax = plt.subplots(
@@ -2426,7 +2426,7 @@ def plot_pie(
     >>>     labels=labels,
     >>>     startangle=90,
     >>>     colors=[
-    >>>         colour_blue, "#33bbee", "#009988", colour_orange, "#cc3311",
+    >>>         colour_blue, colour_cyan, "#009988", colour_orange, "#cc3311",
     >>>         "#ee3377", "#bbbbbb"
     >>>     ]
     >>> )
@@ -2466,7 +2466,7 @@ def plot_stacked_bars(
     width: float = 0.8,
     figsize: Tuple[float, float] = None,
     color: Union[List[str]] = [
-        colour_blue, "#33bbee", "#009988", colour_orange, "#cc3311",
+        colour_blue, colour_cyan, "#009988", colour_orange, "#cc3311",
         "#ee3377", "#bbbbbb"
     ]
 ) -> Tuple[plt.Figure, axes.Axes]:
@@ -2510,7 +2510,7 @@ def plot_stacked_bars(
     figsize : Tuple[float, float] = None
         The (width, height) of the figure (in, in).
     color : str = [
-        colour_blue, "#33bbee", "#009988", colour_orange, "#cc3311",
+        colour_blue, colour_cyan, "#009988", colour_orange, "#cc3311",
         "#ee3377", "#bbbbbb"
     ]
         The color of the bar faces, up to seven levels.
