ee68bfd54e1f3b7b5bc1490e709d95423233edd8
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-04-10 12:40:27 -0500

    Delete ds.despine()

--- scripts/plot_box_and_whisker_plot.py
+++ scripts/plot_box_and_whisker_plot.py
@@ -9,10 +9,10 @@ import datasense as ds
 
 
 def main():
+    ds.style_graph()
     series = ds.random_data()
     fig, ax = ds.plot_boxplot(series=series)
     ax.set_title(label='Box-and-whisker plot')
-    ds.despine(ax=ax)
     ax.set_xticks(ticks=[1], labels=['series'])
     ax.set_ylabel('y')
     fig.savefig(fname=Path('boxplot.svg'))
