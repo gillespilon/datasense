dfaf39ffbc1d7151d13c34944a7678da5df3c066
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-05-04 09:26:05 -0500

    Add sequel.py module

--- datasense/__init__.py
+++ datasense/__init__.py
@@ -2,28 +2,30 @@
 datasense
 
 Statistical and graphical functions for:
-- Analytics
 - Analysing process variation
-- Data science
 - Supervised machine learning
 - Six Sigma methodology
 - Excel file edits
+- Data science
+- Analytics
 
 Why this?
-- Functions to support process control charts.
-- Functions to support measurement system analysis.
-- Functions to simplify statistics, graphs, etc.
-- Develop a free open-source package.
 - Equivalent Python functions that are available in R, SAS, JMP, Minitab
 - Other packages have limited process control analysis features.
 - Other packages are abandoned or inadequately supported.
+- Functions to support measurement system analysis.
+- Functions to simplify statistics, graphs, etc.
+- Functions to support process control charts.
+- Functions to support SQL functionality.
+- Develop a free open-source package.
 '''
 
 
-from .stats import *
 from .control_charts import *
-from .msa import *
 from .munging import *
 from .graphs import *
+from .sequel import *
+from .stats import *
 from .html import *
 from .pyxl import *
+from .msa import *
