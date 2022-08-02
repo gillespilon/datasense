3b0fd582eab02153e174e52a822ba7adf110b282
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-08-02 14:26:28 -0500

    Tidy code with flake8

--- datasense/graphs.py
+++ datasense/graphs.py
@@ -2837,8 +2837,9 @@ def deg_min_sec(decimal_deg_min_sec: float) -> Tuple[int, int, float]:
     -------
     >>> import datasense as ds
     >>> location_decimal = [40.6341666, 14.6027575]
-    >>> location_deg_min_sec = [
-    >>>     ds.deg_min_sec(decimal_deg_min_sec=x) for x in location_decimal
+    >>> location_decimal = [
+    >>>     ds.decimal_degrees(degrees=x, minutes=y, seconds=z)
+    >>>     for x, y, z in location_deg_min_sec
     >>> ]
     >>> [(40, 38, 2.99976), (14, 36, 9.927)]
     """
