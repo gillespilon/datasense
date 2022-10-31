0d3077eede552531e5aa762db93df9899b21a5a6
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-10-31 15:59:15 -0500

    Add test_cubic_spline

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -11,7 +11,15 @@ X = pd.Series(
         16.0, 16.0
     ]
 )
-
+data = {
+    "abscissa": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    "ordinate": [
+        0, 0.841470984807896, 0.909297426825682, 0.141120008059867,
+        -0.756802495307928, -0.958924274663138, -0.279415498198926,
+        0.656986598718789, 0.989358246623382, 0.412118485241757
+    ]
+}
+df = pd.DataFrame(data=data)
 
 # test for method 8
 def test_nonparametric_summary():
@@ -41,3 +49,42 @@ def test_parametric_summary():
     assert series[4] == (14.012638095621575, 22.77197728899381)
     assert series[5] == 7.248
     assert series[6] == 52.527
+
+def test_cubic_spline():
+    cubic_spline = ds.cubic_spline(
+        df=df,
+        abscissa="abscissa",
+        ordinate="ordinate"
+    )
+    result = cubic_spline(x=df["abscissa"])
+    expected = (
+        [
+            0., 0.841470984807896, 0.909297426825682, 0.141120008059867,
+            -0.756802495307928, -0.958924274663138, -0.279415498198926,
+            0.656986598718789, 0.989358246623382, 0.41211848524175704]
+    )
+    assert (result == expected).all()
+
+
+def test_natural_cubic_spline():
+    pass
+
+
+def test_random_data():
+    pass
+
+
+def test_datetime_data():
+    pass
+
+
+def test_timedelta_data():
+    pass
+
+
+def test_two_sample_t():
+    pass
+
+
+def test_linear_regression():
+    pass
