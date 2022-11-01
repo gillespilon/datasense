b3eab700de101c0987e4639dc8f3cd025e36ae79
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-01 11:22:10 -0500

    Add structure for missing tests

---
+++ tests/test_munging.py
@@ -0,0 +1,193 @@
+import datasense as ds
+
+
+def test_listone_contains_all_listtwo_substrings():
+    pass
+
+
+def test_list_directories_within_directory():
+    pass
+
+
+def test_number_empty_cells_in_columns():
+    pass
+
+
+def test_parameters_dict_replacement():
+    pass
+
+
+def test_parameters_text_replacement():
+    pass
+
+
+def test_ask_save_as_file_name_path():
+    pass
+
+
+def test_ask_open_file_name_path():
+    pass
+
+
+def test_find_int_float_columns():
+    pass
+
+
+def test_find_timedelta_columns():
+    pass
+
+
+def test_create_dataframe_norm():
+    pass
+
+
+def test_directory_remove_file():
+    pass
+
+
+def test_replace_column_values():
+    pass
+
+
+def test_feature_percent_empty():
+    pass
+
+
+def test_find_category_columns():
+    pass
+
+
+def test_find_datetime_columns():
+    pass
+
+
+def test_list_one_list_two_ops():
+    pass
+
+
+def test_series_replace_string():
+    pass
+
+
+def test_replace_text_numbers():
+    pass
+
+
+def test_directory_file_print():
+    pass
+
+
+def test_directory_file_list():
+    pass
+
+
+def test_find_object_columns():
+    pass
+
+
+def test_rename_some_columns():
+    pass
+
+
+def test_ask_directory_path():
+    pass
+
+
+def test_rename_all_columns():
+    pass
+
+
+def test_find_float_columns():
+    pass
+
+
+def test_remove_punctuation():
+    pass
+
+
+def test_print_list_by_item():
+    pass
+
+
+def test_find_bool_columns():
+    pass
+
+
+def test_create_dataframe():
+    pass
+
+
+def test_create_directory():
+    pass
+
+
+def test_delete_directory():
+    pass
+
+
+def test_find_int_columns():
+    pass
+
+
+def test_list_change_case():
+    pass
+
+
+def test_rename_directory():
+    pass
+
+
+def test_process_columns():
+    pass
+
+
+def test_copy_directory():
+    pass
+
+
+def test_dataframe_info():
+    pass
+
+
+def test_delete_columns():
+    pass
+
+
+def test_quit_sap_excel():
+    pass
+
+
+def test_mask_outliers():
+    pass
+
+
+def test_process_rows():
+    pass
+
+
+def test_delete_rows():
+    pass
+
+
+def test_byte_size():
+    pass
+
+
+def test_get_mtime():
+    pass
+
+
+def test_file_size():
+    pass
+
+
+def test_read_file():
+    pass
+
+
+def test_save_file():
+    pass
+
+
+def test_sort_rows():
+    pass
