9143610db2d192f80931fc2ff410b148ef79960b
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-01 11:22:17 -0500

    Add structure for missing tests

---
+++ tests/test_html.py
@@ -0,0 +1,41 @@
+import datasense as ds
+
+
+def test_explore_functions():
+    pass
+
+
+def test_sync_directories():
+    pass
+
+
+def test_report_summary():
+    pass
+
+
+def test_script_summary():
+    pass
+
+
+def test_html_figure():
+    pass
+
+
+def test_html_footer():
+    pass
+
+
+def test_html_header():
+    pass
+
+
+def test_html_begin():
+    pass
+
+
+def test_page_break():
+    pass
+
+
+def test_html_end():
+    pass
