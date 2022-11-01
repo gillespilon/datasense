b24c0f19d3153f8668aff198f49b8b48c9433b54
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-01 11:22:27 -0500

    Add structure for missing tests

---
+++ tests/test_pyxl.py
@@ -0,0 +1,81 @@
+import datasense as ds
+
+
+def test_list_empty_except_nan_worksheet_rows():
+    pass
+
+
+def test_list_empty_and_nan_worksheet_rows():
+    pass
+
+
+def test_list_duplicate_worksheet_rows():
+    pass
+
+
+def test_change_case_worksheet_columns():
+    pass
+
+
+def test_write_dataframe_to_worksheet():
+    pass
+
+
+def test_remove_empty_worksheet_rows():
+    pass
+
+
+def test_remove_worksheet_columns():
+    pass
+
+
+def test_list_nan_worksheet_rows():
+    pass
+
+
+def test_list_rows_with_content():
+    pass
+
+
+def test_validate_column_labels():
+    pass
+
+
+def test_number_non_empty_rows():
+    pass
+
+
+def test_remove_worksheet_rows():
+    pass
+
+
+def test_validate_sheet_names():
+    pass
+
+
+def test_autofit_column_width():
+    pass
+
+
+def test_unique_list_items():
+    pass
+
+
+def test_cell_fill_down():
+    pass
+
+
+def test_read_workbook():
+    pass
+
+
+def test_replace_text():
+    pass
+
+
+def test_exit_script():
+    pass
+
+
+def test_cell_style():
+    pass
