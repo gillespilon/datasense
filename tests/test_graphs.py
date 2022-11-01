1f5b3d05fe39d84c3c77564a91120db99c625556
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-01 11:21:59 -0500

    Add structure for missing tests

---
+++ tests/test_graphs.py
@@ -0,0 +1,109 @@
+import datasense as ds
+
+
+def test_plot_scatterleft_scatterright_x_y1_y2():
+    pass
+
+
+def test_plot_scatter_scatter_x1_x2_y1_y2():
+    pass
+
+
+def test_plot_lineleft_lineright_x_y1_y2():
+    pass
+
+
+def test_plot_barleft_lineright_x_y1_y2():
+    pass
+
+
+def test_plot_line_line_line_x_y1_y2_y3():
+    pass
+
+
+def test_plot_scatter_scatter_x_y1_y2():
+    pass
+
+
+def test_plot_scatter_line_x_y1_y2():
+    pass
+
+
+def test_plot_line_line_x_y1_y2():
+    pass
+
+
+def test_plot_horizontal_bars():
+    pass
+
+
+def test_plot_line_line_y1_y2():
+    pass
+
+
+def test_plot_vertical_bars():
+    pass
+
+
+def test_plot_stacked_bars():
+    pass
+
+
+def test_probability_plot():
+    pass
+
+
+def test_plot_scatter_x_y():
+    pass
+
+
+def test_decimal_degrees():
+    pass
+
+
+def test_plot_histogram():
+    pass
+
+
+def test_plot_scatter_y():
+    pass
+
+
+def test_plot_line_x_y():
+    pass
+
+
+def test_format_dates():
+    pass
+
+
+def test_plot_boxplot():
+    pass
+
+
+def test_deg_min_sec():
+    pass
+
+
+def test_plot_line_y():
+    pass
+
+
+def test_plot_pareto():
+    pass
+
+
+def test_style_graph():
+    pass
+
+
+def test_plot_pie():
+    pass
+
+
+def test_despine():
+    pass
+
+
+def test_qr_code():
+    pass
