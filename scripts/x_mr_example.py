1225c9d186f0757d9958f39a830d9ed67dbba030
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-06 19:04:45 -0600

    Correct fig error

--- scripts/x_mr_example.py
+++ scripts/x_mr_example.py
@@ -2,7 +2,7 @@
 """
 Example of XmR control charts
 
-time -f '%e' ./x_mr_example.py
+time -f "%e" ./x_mr_example.py
 ./x_mr_example.py
 """
 
@@ -14,18 +14,18 @@ import matplotlib.pyplot as plt
 import datasense as ds
 import pandas as pd
 
-data_file = 'x_mr_example'
-x_chart_title = 'Individuals Control Chart'
-x_chart_ylabel = 'Measurement X (units)'
-x_chart_xlabel = 'Sample'
-mr_chart_title = 'Moving Range Control Chart'
-mr_chart_ylabel = 'Measurement mR (units)'
-mr_chart_xlabel = 'Sample'
-colour = '#33bbee'
+mr_chart_title = "Moving Range Control Chart"
+x_chart_title = "Individuals Control Chart"
+mr_chart_ylabel = "Measurement mR (units)"
+x_chart_ylabel = "Measurement X (units)"
+output_url = "x_mr_example.html"
+header_title = "x_mr_example"
+header_id = "x-mr-example"
+mr_chart_xlabel = "Sample"
+data_file = "x_mr_example"
+x_chart_xlabel = "Sample"
+colour = "#33bbee"
 figsize = (8, 6)
-output_url = 'x_mr_example.html'
-header_title = 'x_mr_example'
-header_id = 'x-mr-example'
 
 
 def main():
@@ -53,53 +53,55 @@ def main():
 
 
 def create_data() -> pd.DataFrame:
-    '''
+    """
     Creates a dataframe.
-    '''
+    """
     df = pd.DataFrame(
         {
-            'Sample':  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-            'X':       [25.0, 24.0, 38.5, 22.4, 23.1, 13.9, 13.9,
-                        10.0, 13.3, 10.0, 16.0, 16.0, 16.0]
+            "Sample":  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+            "X":       [
+                25.0, 24.0, 38.5, 22.4, 23.1, 13.9, 13.9, 10.0, 13.3, 10.0,
+                16.0, 16.0, 16.0
+            ]
         }
-    ).set_index('Sample')
+    ).set_index("Sample")
     return df
 
 
 def x_chart(df: pd.DataFrame) -> NoReturn:
-    '''
+    """
     Creates an X control chart.
     Identifies out-of-control points.
     Adds chart and axis titles.
     Saves the figure in svg format.
-    '''
+    """
     fig = plt.figure(figsize=figsize)
     x = cc.X(data=df)
-    ax = x.ax(fig)
+    ax = x.ax(fig=fig)
     ax.axhline(
         y=x.sigmas[+1],
-        linestyle='--',
+        linestyle="--",
         dashes=(5, 5),
         color=colour,
         alpha=0.5
     )
     ax.axhline(
         y=x.sigmas[-1],
-        linestyle='--',
+        linestyle="--",
         dashes=(5, 5),
         color=colour,
         alpha=0.5
     )
     ax.axhline(
         y=x.sigmas[+2],
-        linestyle='--',
+        linestyle="--",
         dashes=(5, 5),
         color=colour,
         alpha=0.5
     )
     ax.axhline(
         y=x.sigmas[-2],
-        linestyle='--',
+        linestyle="--",
         dashes=(5, 5),
         color=colour,
         alpha=0.5
@@ -107,50 +109,55 @@ def x_chart(df: pd.DataFrame) -> NoReturn:
     cc.draw_rules(x, ax)
     ax.set_title(
         label=x_chart_title,
-        fontweight='bold'
+        fontweight="bold"
     )
     ax.set_ylabel(ylabel=x_chart_ylabel)
     ax.set_xlabel(xlabel=x_chart_xlabel)
-    fig.savefig(fname=f'{data_file}_x.svg')
-    ds.html_figure(file_name=f'{data_file}_x.svg')
+    fig.savefig(fname=f"{data_file}_x.svg")
+    ds.html_figure(file_name=f"{data_file}_x.svg")
     print(
-       f'X Report\n'
-       f'===================\n'
-       f'UCL        : {x.ucl.round(3)}\n'
-       f'Xbar       : {x.mean.round(3)}\n'
-       f'LCL        : {x.lcl.round(3)}\n'
-       f'Sigma(X)   : {x.sigma.round(3)}\n'
+       f"X Report\n"
+       f"===================\n"
+       f"UCL        : {x.ucl.round(3)}\n"
+       f"Xbar       : {x.mean.round(3)}\n"
+       f"LCL        : {x.lcl.round(3)}\n"
+       f"Sigma(X)   : {x.sigma.round(3)}\n"
     )
 
 
 def mr_chart(df: pd.DataFrame) -> NoReturn:
-    '''
+    """
     Creates an mR control chart.
     Identifies out-of-control points.
     Adds chart and axis titles.
     Saves the figure in svg format.
-    '''
+    """
     fig = plt.figure(figsize=figsize)
     mr = cc.mR(data=df)
-    ax = mr.ax(fig)
-    cc.draw_rule(mr, ax, *cc.points_one(mr), '1')
+    ax = mr.ax(fig=fig)
+    cc.draw_rule(
+        mr,
+        ax,
+        *cc.points_one(mr),
+        "1"
+    )
     ax.set_title(
         label=mr_chart_title,
-        fontweight='bold'
+        fontweight="bold"
     )
     ax.set_ylabel(ylabel=mr_chart_ylabel)
     ax.set_xlabel(xlabel=mr_chart_xlabel)
-    fig.savefig(fname=f'{data_file}_mr.svg')
-    ds.html_figure(file_name=f'{data_file}_mr.svg')
+    fig.savefig(fname=f"{data_file}_mr.svg")
+    ds.html_figure(file_name=f"{data_file}_mr.svg")
     print(
-       f'mR Report\n'
-       f'===================\n'
-       f'UCL        : {mr.ucl.round(3)}\n'
-       f'mRbar      : {mr.mean.round(3)}\n'
-       f'LCL        : {round(mr.lcl, 3)}\n'
-       f'Sigma(mR)  : {mr.sigma.round(3)}\n'
+       f"mR Report\n"
+       f"===================\n"
+       f"UCL        : {mr.ucl.round(3)}\n"
+       f"mRbar      : {mr.mean.round(3)}\n"
+       f"LCL        : {round(mr.lcl, 3)}\n"
+       f"Sigma(mR)  : {mr.sigma.round(3)}\n"
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
