86e076f9dd1a297dae0bfaff48d2e3f0ec253c36
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-13 17:36:09 -0600

    Create cron_script.py

---
+++ tests/cron_script.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+"""
+Execute pytest for datasense
+"""
+
+from pathlib import Path
+from os import chdir
+import webbrowser
+import pytest
+
+
+def main():
+    chdir(Path(__file__).parent.resolve())  # required for cron
+    url = "pytest.html"
+    pytest.main(["--html=pytest.html"])
+    webbrowser.open_new_tab(url=url)
+
+
+if __name__ == "__main__":
+    main()
