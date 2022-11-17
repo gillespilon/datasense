6e30bd453982d5dcf52b55c4a0e1219699d586ce
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-17 16:56:39 -0600

    Revise expected results for two-sample t

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -220,9 +220,11 @@ def test_one_sample_t():
         alternative_hypothesis="two-sided"
     )
     # expected = (
-    # t statistic, t p value, t power, Shapiro-Wilk statistic, Shapiro-Wilk
-    # p value, Anderson-Darling statistic, Anderson-Darling critical value for
-    # alpha 0.05, Kolmogorov-Smirnov statistic, Kolmogorov-Smirnov p value,
+    # t statistic, t p value, t power,
+    # Shapiro-Wilk statistic, Shapiro-Wilk p value,
+    # Anderson-Darling statistic, Anderson-Darling critical value for
+    # alpha 0.05,
+    # Kolmogorov-Smirnov statistic, Kolmogorov-Smirnov p value,
     # CI lower bound, CI upper bound
     # )
     expected = (
@@ -266,26 +268,42 @@ def test_two_sample_t():
         series1=series1_two_sample_t,
         series2=series2_two_sample_t,
         alternative_hypothesis="two-sided",
-        significance_level=0.05
+        significance_level=0.05,
     )
-    # expected = (statistic, p value, power)
-    expected = (2.206697123558633, 0.040563312956175504, 0.5510566836848744)
+    # expected = (
+    # t statistic, t p value, t power,
+    # Shapiro-Wilk statistic sample 1, Shapiro-Wilk p value sample 1,
+    # Shapiro-Wilk statistic sample 2, Shapiro-Wilk p value sample 2
+    # )
+    expected = (
+        2.206697123558633, 0.040563312956175504, 0.5510566836848744,
+        0.9662597179412842, 0.8464781045913696,
+        0.8837727308273315, 0.17198464274406433
+    )
     assert result == expected
     result = ds.two_sample_t(
         series1=series1_two_sample_t,
         series2=series2_two_sample_t,
         alternative_hypothesis="less",
-        significance_level=0.05
+        significance_level=0.05,
     )
-    expected = (2.206697123558633, 0.9797183435219122, 8.068361761393895e-05)
+    expected = (
+        2.206697123558633, 0.9797183435219122, 8.068361761393895e-05,
+        0.9662597179412842, 0.8464781045913696,
+        0.8837727308273315, 0.17198464274406433
+    )
     assert result == expected
     result = ds.two_sample_t(
         series1=series1_two_sample_t,
         series2=series2_two_sample_t,
         alternative_hypothesis="greater",
-        significance_level=0.05
+        significance_level=0.05,
     )
-    expected = (2.206697123558633, 0.020281656478087752, 0.6835840521967709)
+    expected = (
+        2.206697123558633, 0.020281656478087752, 0.6835840521967709,
+        0.9662597179412842, 0.8464781045913696,
+        0.8837727308273315, 0.17198464274406433
+    )
     assert result == expected
 
 
