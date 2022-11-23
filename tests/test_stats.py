4f992969b2422e5232c2b0ddbb9af9687979652e
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-23 14:26:44 -0600

    Add test for paired t

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -62,6 +62,20 @@ df_linear_regression = pd.DataFrame(
         ]
     }
 )
+series1_paired = pd.Series(
+    data=[
+        68, 76, 74, 71, 71, 72, 75, 83, 75, 74, 76, 77, 78, 75, 75, 84, 77,
+        69, 75, 65
+    ],
+    name="before"
+)
+series2_paired = pd.Series(
+    data=[
+        67, 77, 74, 74, 69, 70, 71, 77, 71, 74, 73, 68, 71, 72, 77, 80, 74,
+        73, 72, 62
+    ],
+    name="after"
+)
 
 
 def test_nonparametric_summary():
@@ -315,6 +329,53 @@ def test_two_sample_t():
         3.2744574205759416, 0.07036619072494953,
         0.15265397324961683, 0.68, 0.49940696863048295, 0.693,
         0.06234516845619442, 7.67502856891755
+    )
+    assert result == expected
+
+
+def test_paired_t():
+    result = ds.paired_t(
+        series1=series1_paired,
+        series2=series2_paired,
+        significance_level=0.05,
+        alternative_hypothesis="two-sided",
+    )
+    # expected = (
+    # t statistic, t p value, t power,
+    # Shapiro-Wilk statistic, Shapiro-Wilk p value,
+    # Anderson-Darling test statistic, Anderson-Darling p value
+    # )
+    expected = (
+        3.0234339882840073, 0.006989193823492975,
+        0.9718567132949829, 0.7935113906860352,
+        0.33171651643374744, 0.692,
+        0.14709700945048904, 0.3071349516596349
+    )
+    assert result == expected
+    result = ds.paired_t(
+        series1=series1_paired,
+        series2=series2_paired,
+        significance_level=0.05,
+        alternative_hypothesis="less"
+    )
+    expected = (
+        3.0234339882840073, 0.9965054030882535,
+        0.9718567132949829, 0.7935113906860352,
+        0.33171651643374744, 0.692,
+        0.14709700945048904, 0.3071349516596349
+    )
+    assert result == expected
+    result = ds.paired_t(
+        series1=series1_paired,
+        series2=series2_paired,
+        significance_level=0.05,
+        alternative_hypothesis="greater"
+    )
+    expected = (
+        3.0234339882840073, 0.0034945969117464873,
+        0.9718567132949829, 0.7935113906860352,
+        0.33171651643374744, 0.692,
+        0.14709700945048904, 0.3071349516596349
     )
     assert result == expected
 
