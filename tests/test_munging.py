37872b812944c365059e0b4fe287cd9770f28754
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-12-14 11:25:33 -0600

    Add assertion for case where not all columns in list are empty

--- tests/test_munging.py
+++ tests/test_munging.py
@@ -198,6 +198,56 @@ def test_delete_empty_columns():
         }
     )
     assert result2.equals(other=expected2)
+    # No not delete columns using list_empty_columns because
+    # not all columns in list are empty
+    list_empty_columns = ["mixed", "nan_none", "integers"]
+    result3 = ds.delete_empty_columns(
+        df=df_empty_test,
+        list_empty_columns=list_empty_columns
+    )
+    expected3 = pd.DataFrame(
+        data=dict(
+            floats=[1.0, np.NaN, 3.0, np.NaN, 5.0, 6.0, np.NaN],
+            text=["A", "B", "C", "D", "E", "F", np.NaN],
+            dates=[
+                "1956-06-08", "1956-06-08",
+                "1956-06-08", "1956-06-08",
+                "1956-06-08", "1956-06-08",
+                pd.NaT
+            ],
+            all_nan=[np.NaN, np.NaN, np.NaN, np.NaN, np.NaN, np.NaN, np.NaN],
+            all_nat=[pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT],
+            all_none=[None, None, None, None, None, None, None],
+            all_space=["", " ", "", " ", "", "", ""],
+            nan_space=[np.NaN, "", " ", np.NaN, np.NaN, np.NaN, np.NaN],
+            nan_none=[np.NaN, None, np.NaN, np.NaN, None, np.NaN, None],
+            mixed=[None, np.NaN, pd.NaT, pd.NaT, None, np.NaN, pd.NaT],
+            integers=[1, 2, np.NaN, 4, 5, 6, np.NaN],
+        )
+    ).replace(
+        r"^\s+$",
+        np.NaN,
+        regex=True
+    ).replace(
+        "",
+        np.NaN,
+        regex=True
+    ).astype(
+        dtype={
+            "integers": "Int64",
+            "floats": "float64",
+            "text": "object",
+            "dates": "datetime64[ns]",
+            "all_nan": "float64",
+            "all_nat": "datetime64[ns]",
+            "all_none": "float64",
+            "all_space": "float64",
+            "nan_space": "float64",
+            "nan_none": "float64",
+            "mixed": "datetime64[ns]"
+        }
+    )
+    assert result3.equals(other=expected3)
 
 
 def test_replace_text_numbers():
