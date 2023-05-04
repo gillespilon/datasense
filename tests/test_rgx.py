482386143b7b11dc3c25a60cff0d745f1b5c69a1
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-05-04 13:22:42 -0500

    Add test_rgx_url()

--- tests/test_rgx.py
+++ tests/test_rgx.py
@@ -27,3 +27,34 @@ def test_rgx_email_address():
     expected = list(set(data.split("\n")))
     expected = [i for i in expected if i]
     expected.sort()
+
+
+def test_rgx_url():
+    list_strings = [
+        "https://www.google.com",
+        "https://www.wikipedia.org/",
+        "http://www.wikipedia.org/",
+        "one https://www.wikipedia.org/ and two http://www.wikipedia.org/",
+        "http://localhost:631/jobs/",
+        "https://en.wikipedia.org/wiki/Regular_expression#History",
+        "www.regexbuddy.com",
+        "http://www.regexbuddy.com/index.html",
+        "http://www.regexbuddy.com/index.html?source=library",
+        "Download RegexBuddy at http://www.regexbuddy.com/download.html",
+        "http://10.2.2.1.2/ttxx/txt/gg",
+        "file:///home/gilles/downloads/cheat_sheet_finance.pdf"
+    ]
+    result = ds.rgx_url(strings=list_strings)
+    expected = [
+        'file:///home/gilles/downloads/cheat_sheet_finance.pdf',
+        'http://10.2.2.1.2/ttxx/txt/gg',
+        'http://localhost:631/jobs/',
+        'http://www.regexbuddy.com/download.html',
+        'http://www.regexbuddy.com/index.html',
+        'http://www.regexbuddy.com/index.html?source=library',
+        'http://www.wikipedia.org/',
+        'https://en.wikipedia.org/wiki/Regular_expression#History',
+        'https://www.google.com',
+        'https://www.wikipedia.org/'
+     ]
+    assert result == expected
