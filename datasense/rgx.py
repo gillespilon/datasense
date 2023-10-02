7518ea7a8ff848ad55f08e901897234906e0fbb8
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-10-02 10:10:16 -0500

    Fix example in def rgx_url for doctest

--- datasense/rgx.py
+++ datasense/rgx.py
@@ -21,32 +21,7 @@ def rgx_email_address(
     Returns
     -------
     matches : List[str]
-        A list of strings containing the email addresses found in the input
-        list.
-
-    Examples
-    --------
-    Example 1
-    >>> import datasense as ds
-    >>> strings = [
-    >>>     "first email bob.smith@somemail.com send",
-    >>>     "second email bobsmith13@othermail.com received",
-    >>>     "third email tom@onemail.com and fourth mail frank@twomail.com"
-    >>> ]
-    >>> matches = ds.rgx_email_address(strings=strings)
-    >>> print(matches)
-    [
-        'bob.smith@somemail.com', 'bobsmith13@othermail.com',
-        'tom@onemail.com', 'frank@twomail.com'
-    ]
-
-    Example 2
-    # open a file containing email addresses and \n\t
-    >>> with open('mailbox.txt') as f:
-    >>>     data = f.read()
-    >>> strings = data.split("\n")
-    >>> matches = ds.rgx_email_address(strings=strings)
-    >>> print(matches)
+        A list of strings containing the email addresses in the input list.
     """
     regex = re.compile(pattern=r"[\w.-]+@[\w.-]+")
     list_of_lists = [re.findall(pattern=regex, string=x) for x in strings]
@@ -79,36 +54,34 @@ def rgx_url(
     Example
     -------
     Example
+    -------
     >>> import datasense as ds
     >>> strings = [
-    >>> "https://www.google.com",
-    >>> "https://www.wikipedia.org/",
-    >>> "http://www.wikipedia.org/",
-    >>> "one https://www.wikipedia.org/ and two http://www.wikipedia.org/",
-    >>> "http://localhost:631/jobs/",
-    >>> "https://en.wikipedia.org/wiki/Regular_expression#History",
-    >>> "www.regexbuddy.com",
-    >>> "http://www.regexbuddy.com/index.html",
-    >>> "http://www.regexbuddy.com/index.html?source=library",
-    >>> "Download RegexBuddy at http://www.regexbuddy.com/download.html",
-    >>> "http://10.2.2.1.2/ttxx/txt/gg",
-    >>> "file:///home/gilles/downloads/cheat_sheet_finance.pdf"
-    >>> ]
-    >>> matches = ds.rgx_email_address(strings=strings)
-    >>> print(matches)
-    [
-        'file:///home/gilles/downloads/cheat_sheet_finance.pdf',
-         'http://10.2.2.1.2/ttxx/txt/gg',
-         'http://localhost:631/jobs/',
-         'http://www.regexbuddy.com/download.html',
-         'http://www.regexbuddy.com/index.html',
-         'http://www.regexbuddy.com/index.html?source=library',
-         'http://www.wikipedia.org/',
-         'https://en.wikipedia.org/wiki/Regular_expression#History',
-         'https://www.google.com',
-         'https://www.wikipedia.org/'
-     ]
-
+    ...     "https://www.google.com",
+    ...     "https://www.wikipedia.org/",
+    ...     "http://www.wikipedia.org/",
+    ...     "one https://www.wikipedia.org/ and two http://www.wikipedia.org/",
+    ...     "http://localhost:631/jobs/",
+    ...     "https://en.wikipedia.org/wiki/Regular_expression#History",
+    ...     "www.regexbuddy.com",
+    ...     "http://www.regexbuddy.com/index.html",
+    ...     "http://www.regexbuddy.com/index.html?source=library",
+    ...     "Download RegexBuddy at http://www.regexbuddy.com/download.html",
+    ...     "http://10.2.2.1.2/ttxx/txt/gg",
+    ...     "file:///home/gilles/downloads/cheat_sheet_finance.pdf"
+    ... ]
+    >>> matches = ds.rgx_url(strings=strings)
+    >>> matches # doctest: +NORMALIZE_WHITESPACE
+    ['file:///home/gilles/downloads/cheat_sheet_finance.pdf',
+     'http://10.2.2.1.2/ttxx/txt/gg',
+     'http://localhost:631/jobs/',
+     'http://www.regexbuddy.com/download.html',
+     'http://www.regexbuddy.com/index.html',
+     'http://www.regexbuddy.com/index.html?source=library',
+     'http://www.wikipedia.org/',
+     'https://en.wikipedia.org/wiki/Regular_expression#History',
+     'https://www.google.com',
+     'https://www.wikipedia.org/']
     """
     regex = re.compile(
         r"""
@@ -116,9 +89,9 @@ def rgx_url(
         (?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?(?:\.|/|_))+
         (?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|
         localhost|
-        \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|  # ...or ipv4
-        \[?[A-F0-9]*:[A-F0-9:]+\]?)          # ...or ipv6
-        (?::\d+)?                            # optional port
+        \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|
+        \[?[A-F0-9]*:[A-F0-9:]+\]?)
+        (?::\d+)?
         (?:/?|[/?]\S+)$
         """,
         flags=re.IGNORECASE | re.VERBOSE
