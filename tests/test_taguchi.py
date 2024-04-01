359b873a3b26353200a844a9d7264605a0846143
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-04-01 10:00:25 -0500

    Create test_taguchi.py, test_taguchi_loss_function

---
+++ tests/test_taguchi.py
@@ -0,0 +1,21 @@
+import datasense as ds
+
+
+def test_taguchi_loss_function():
+    """
+    Calculate the ACU for the Taguchi Loss Function.
+    """
+    average = 4.66
+    std_dev = 1.80
+    target = 7.5
+    cost = 0.25
+    x = 15
+    result = ds.taguchi_loss_function(
+        average=average,
+        std_dev=std_dev,
+        target=target,
+        cost=cost,
+        x=x,
+    )
+    expected = 0.05024711111111111
+    assert result == expected
