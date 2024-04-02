1f1bd62a1981f205713efc447432fb5b6bc716b2
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-04-02 15:10:50 -0500

    Add content for def pp()

--- datasense/process_capability.py
+++ datasense/process_capability.py
@@ -8,6 +8,10 @@ standard (specification). A process is capable if you have:
 - Process is on target with minimum dispersion.
 """
 
+import math
+
+from scipy.stats import chi2
+
 
 def cp() -> None:
     """
@@ -42,15 +46,73 @@ def cpm() -> None:
     pass
 
 
-def pp() -> None:
+def pp(
+    average: float | int,
+    std_devn: float | int,
+    sample_size: int,
+    lower_spec: float | int,
+    upper_spec: float | int,
+    alpha: float = 0.05,
+) -> tuple[float, float, float]:
     """
     Pp compares the width of the process specification to the width of the
     process variation. It does not take into consideration the deviation from
     the average. It "assumes" the process is centred between the specification
     limits. The standard deviation uses the "sample standard deviation"
     formula.
+
+    Parameters
+    ----------
+    average : float | int,
+        The average of the process.
+    std_devn : float | int,
+        The standard deviation of the process. It should be the "sample
+        standard deviation".
+    sample_size : int,
+        This is the sample size for the data being analysed.
+    lower_spec : float | int,
+        The lower specification value.
+    upper_spec : float | int,
+        The upper specification value.
+    alpha : float = 0.05
+        The alpha value for the confidence interval calculations. An alpha of
+        0.05 is used for a 95 % confidence interval.
+
+    Returns
+    -------
+    capability : float
+        The Pp process capability value.
+    lower_bound : float
+        The lower value of the confidence interval for Pp.
+    upper_bound : float
+        The upper value of the confidence interval for Pp.
+
+    Example
+    -------
+    >>> import datasense as ds
+    >>> average = 0.11001
+    >>> std_devn = 0.868663
+    >>> sample_size = 40
+    >>> lower_spec = -4
+    >>> upper_spec = 4
+    >>> alpha = 0.05
+    >>> result = ds.pp(
+    >>>     average=average,
+    >>>     std_devn=std_devn,
+    >>>     sample_size=sample_size,
+    >>>     lower_spec=lower_spec,
+    >>>     upper_spec=upper_spec,
+    >>>     alpha=alpha
+    >>> )
+    (1.5349258956964131, 1.1953921108301047, 1.873778000024199)
     """
-    pass
+    capability = (upper_spec - lower_spec) / (6 * std_devn)
+    degrees_of_freedom = sample_size - 1
+    chi2_lower = chi2.ppf(q=alpha / 2, df=degrees_of_freedom)
+    chi2_upper = chi2.ppf(q=1 - alpha / 2, df=degrees_of_freedom)
+    lower_bound = capability * math.sqrt(chi2_lower / degrees_of_freedom)
+    upper_bound = capability * math.sqrt(chi2_upper / degrees_of_freedom)
+    return (capability, lower_bound, upper_bound)
 
 
 def ppk() -> None:
