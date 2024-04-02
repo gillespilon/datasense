5f1121ee13fca74adefa8a2dbd694e3a4e7c328e
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-04-02 15:12:22 -0500

    Add test for centred process for test_taguchi_loff_function

--- tests/test_taguchi.py
+++ tests/test_taguchi.py
@@ -5,6 +5,7 @@ def test_taguchi_loss_function():
     """
     Calculate the ACU for the Taguchi Loss Function.
     """
+    # off-centred process
     average = 4.66
     std_dev = 1.80
     target = 7.5
@@ -18,4 +19,18 @@ def test_taguchi_loss_function():
         x=x,
     )
     expected = 0.05024711111111111
+    # centred process
+    average = 7.5
+    std_dev = 1.80
+    target = 7.5
+    cost = 0.25
+    x = 15
+    result = ds.taguchi_loss_function(
+        average=average,
+        std_dev=std_dev,
+        target=target,
+        cost=cost,
+        x=x,
+    )
+    expected = 0.014400000000000001
     assert result == expected
