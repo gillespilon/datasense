606791fa04cc6182e8c9c9d097e53b51181b0bc3
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-04-05 15:08:29 -0500

    Add def test_ppk()

--- tests/test_process_capability.py
+++ tests/test_process_capability.py
@@ -18,3 +18,26 @@ def test_pp():
     )
     expected = (1.5349258956964131, 1.1953921108301047, 1.873778000024199)
     assert result == expected
+
+
+def test_ppk():
+    average = 0.11001
+    std_devn = 0.868663
+    sample_size = 40
+    lower_spec = -4
+    upper_spec = 4
+    alpha = 0.05
+    result = ds.ppk(
+        average=average,
+        std_devn=std_devn,
+        sample_size=sample_size,
+        lower_spec=lower_spec,
+        upper_spec=upper_spec,
+        alpha=alpha,
+        toler=6,
+    )
+    expected = (
+        1.4927115962500226, 1.5771401951428037, 1.4927115962500226,
+        1.1457133294762083, 1.8397098630238369
+    )
+    assert result == expected
