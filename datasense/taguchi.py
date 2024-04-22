a8643bcd3b2e26711d31af9cabdfc79ab812e08f
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-04-22 14:01:07 -0500

    Update docstrings for Sphinx

--- datasense/taguchi.py
+++ datasense/taguchi.py
@@ -17,30 +17,30 @@ def taguchi_loss_function(
 
     Parameters
     ----------
-    average : float
+    average: float | int
         It is the average measurement value for the product stream. It is best
         to use a control chart (Xbar R | X mR) to estimate the average.
-    std_dev : float
+    std_dev: float | int
         It is the standard deviation of the product stream. It is best to use
         a control chart (Xbar R | X mR) to estimate the average.
-    target : float
+    target: float | int
         It is the target value of the product stream.
-    cost : float
-        It is the cost of scrap, rework, or repair for a single unit of
-        product.
-    x : float
+    cost: float | int
+        It is the cost of scrap, rework, or repair for a single unit of product.
+    x: float | int
         It is the measurement value at which an item is scrapped, reworked, or
         repaired.
 
-    Returns:
-    --------
-    acu : float
+    Returns
+    -------
+    acu: float
+        The average cost of use.
 
     Examples
     --------
-    Example 1
-    ---------
+
     Calculate ACU for an off-centred process with LS and US.
+
     >>> import datasense as ds
     >>> average = 4.66
     >>> std_dev = 1.80
@@ -57,9 +57,8 @@ def taguchi_loss_function(
     >>> acu
     0.05024711111111111
 
-    Example 2
-    ---------
     Calculate ACU for a centred process with LS and US.
+
     >>> import datasense as ds
     >>> average = 7.5
     >>> std_dev = 1.80
@@ -80,7 +79,7 @@ def taguchi_loss_function(
     k = cost / (x - target) ** 2
     # Calculate the ACU.
     acu = k * (std_dev ** 2 + (average - target) ** 2)
-    return acu
+    return (acu, k)
 
 
 __all__ = (
