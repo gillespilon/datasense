67f3db1681f3ee05e3f61db906f6634d52cadd5d
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-04-24 14:45:02 -0500

    Add two tests dd_to_dms, dms_to_dd

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -57,10 +57,6 @@ def test_plot_scatter_x_y():
     pass
 
 
-def test_decimal_degrees():
-    pass
-
-
 def test_plot_histogram():
     pass
 
@@ -81,10 +77,6 @@ def test_plot_boxplot():
     pass
 
 
-def test_deg_min_sec():
-    pass
-
-
 def test_plot_line_y():
     pass
 
@@ -95,6 +87,43 @@ def test_plot_pareto():
 
 def test_style_graph():
     pass
+
+
+def test_dd_to_dms():
+    dd = [45.4250225, -75.6970594]
+    result = ds.dd_to_dms(dd=dd)
+    expected = [(45, 25, 30.081, 'N'), (75, 41, 49.41384, 'W')]
+    assert result == expected
+    dd = [48.858393, 2.257616]
+    result = ds.dd_to_dms(dd=dd)
+    expected = [(48, 51, 30.2148, 'N'), (2, 15, 27.4176, 'E')]
+    assert result == expected
+    dd = [-13.163194, -72.547842]
+    result = ds.dd_to_dms(dd=dd)
+    expected = [(13, 9, 47.4984, 'S'), (72, 32, 52.2312, 'W')]
+    assert result == expected
+    dd = [-33.8567433, 151.1784306]
+    result = ds.dd_to_dms(dd=dd)
+    expected = [(33, 51, 24.27588, 'S'), (151, 10, 42.35016, 'E')]
+    assert result == expected
+
+def test_dms_to_dd():
+    dms = [(45, 25, 30.081, 'N'), (75, 41, 49.41384, 'W')]
+    result = ds.dms_to_dd(dms=dms)
+    expected = [45.4250225, -75.6970594]
+    assert result == expected
+    dms = [(48, 51, 30.2148, 'N'), (2, 15, 27.4176, 'E')]
+    result = ds.dms_to_dd(dms=dms)
+    expected = [48.858393, 2.257616]
+    assert result == expected
+    dms = [(13, 9, 47.4984, 'S'), (72, 32, 52.2312, 'W')]
+    result = ds.dms_to_dd(dms=dms)
+    expected = [-13.163194, -72.547842]
+    assert result == expected
+    dms = [(33, 51, 24.27588, 'S'), (151, 10, 42.35016, 'E')]
+    result = ds.dms_to_dd(dms=dms)
+    expected = [-33.8567433, 151.1784306]
+    assert result == expected
 
 
 def test_plot_pie():
