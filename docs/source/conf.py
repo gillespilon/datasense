2a7200ba203f7fd2a9dfbd1dd513ba7e5882df4a
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-04-29 12:38:14 -0500

    Add line for virtual environment of python site-packages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../datasense'))
+sys.path.insert(0, '/home/gilles/documents/repositories/virtual/venv/lib/python3.12/site-packages')
 
 # Configuration file for the Sphinx documentation builder.
 # For the full list of built-in configuration values, see the documentation:
